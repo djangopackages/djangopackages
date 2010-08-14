3f54c76b97574e08839051820d2b88fd773a0e22
pydanny <pydanny@gmail.com>
2010-08-14 06:04:56 -0500

    Added categories to packages. Also initial_data for package fixtures to provide base content and help with testing

--- rabbits/apps/package/admin.py
+++ rabbits/apps/package/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from package.models import Package, PackageExample
+from package.models import Category, Package, PackageExample
 
-
+admin.site.register(Category)
 admin.site.register(Package)
 admin.site.register(PackageExample)
