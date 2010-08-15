052298df4a42978045389f1d2909daafa89c3b0f
pydanny <pydanny@gmail.com>
2010-08-14 23:31:59 -0500

    Working on the homepage app

---
+++ rabbits/apps/homepage/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from homepage.models import Dpotw, Gotw
+
+admin.site.register(Dpotw)
+admin.site.register(Gotw)
---
+++ rabbits/apps/homepage/models.py
@@ -0,0 +1,47 @@
+import datetime
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _ 
+
+from grid.models import Grid
+from package.models import BaseModel, Package
+
+class RotatorManager(models.Manager):
+    
+    def get_current(self):
+        now = datetime.datetime.now()
+        return self.get_query_set().filter(start_date__lte=now,end_date__gte=now)
+
+class Dpotw(BaseModel):
+    
+    package = models.ForeignKey(Package)
+    start_date = models.DateField(_("Start Date"))
+    end_date = models.DateField(_("End Date"))    
+    
+    objects = RotatorManager()
+    
+    class Meta:
+        
+        verbose_name         = "Django Package of the Week"
+        verbose_name_plural  = "Django Packages of the Week"
+        
+    def __unicode__(self):
+        return '%s : %s - %s' % (self.package.title, self.start_date, self.end_date)
+
+class Gotw(BaseModel):
+    
+    grid = models.ForeignKey(Grid)    
+    
+    start_date = models.DateField(_("Start Date"))
+    end_date = models.DateField(_("End Date"))        
+    
+    objects = RotatorManager()    
+    
+    class Meta:
+        
+        verbose_name         = "Grid of the Week"
+        verbose_name_plural  = "Grids of the Week"
+    
+    def __unicode__(self):
+        return '%s : %s - %s' % (self.grid.title, self.start_date, self.end_date)
+    
