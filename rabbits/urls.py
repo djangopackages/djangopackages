5ad192c7c42256c16a3d16a64189abc67455c8c1
pydanny <pydanny@gmail.com>
2010-08-15 14:00:37 -0500

    adding in terms of service

--- rabbits/urls.py
+++ rabbits/urls.py
@@ -20,8 +20,6 @@ urlpatterns = patterns("",
     
     url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
     url(r"^admin/", include(admin.site.urls)),
-    
-    # TODO - flesh out the about pages
     url(r"^about/", include("about.urls")),
     url(r"^account/", include("pinax.apps.account.urls")),
     url(r"^openid/(.*)", PinaxConsumer()),
