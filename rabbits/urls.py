f83323e249a69c58c3f6c48f7e2a50d022c96b26
pydanny <pydanny@gmail.com>
2010-08-14 19:07:24 -0500

    Looping for fun and profit

--- rabbits/urls.py
+++ rabbits/urls.py
@@ -1,19 +1,22 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from django.views.generic.list_detail import object_list
 
 from django.contrib import admin
 admin.autodiscover()
 
 from pinax.apps.account.openid_consumer import PinaxConsumer
 
+from package.models import Package
 
 handler500 = "pinax.views.server_error"
 
 
 urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {
-        "template": "homepage.html",
+    url(r"^$", object_list, {
+        "template_name": "homepage.html",
+        "queryset":Package.objects.all()
     }, name="home"),
     url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
     url(r"^admin/", include(admin.site.urls)),
