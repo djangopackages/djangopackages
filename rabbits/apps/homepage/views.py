e3a8af605976ac38423d3c18d0cab3ef4f3754ab
pydanny <pydanny@gmail.com>
2010-08-15 00:05:51 -0500

    Changing homepage.html to be a view driven by homepage app

--- rabbits/apps/homepage/views.py
+++ rabbits/apps/homepage/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.shortcuts import render_to_response, get_object_or_404 
+from django.template import RequestContext 
+from django.views.decorators.csrf import csrf_protect
+
+
+from package.models import Package
+from homepage.models import Dpotw, Gotw, Tab
+
+def homepage(request, template_name="homepage.html"):
+    
+    return render_to_response(template_name, {
+        'packages': Package.objects.all(),
+        'dpotw': Dpotw.objects.get_current(),
+        'gotw': Gotw.objects.get_current(),
+        'tab': Tab.objects.all()
+        },
+        context_instance=RequestContext(request)
+        )
