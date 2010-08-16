f5d0b192bdb57e1732da113b6b176252dae370a9
pydanny <pydanny@gmail.com>
2010-08-15 21:09:16 -0500

    update_packages lookup

--- rabbits/apps/package/management/commands/update_packages.py
+++ rabbits/apps/package/management/commands/update_packages.py
@@ -1,4 +1,4 @@
-from string import ascii_letters
+from string import ascii_lowercase
 from sys import stdout
 import threading
 
@@ -12,17 +12,21 @@ class Command(NoArgsCommand):
     
     def handle(self, *args, **options):
 
+        count = 0
         threads = []
-        for letter in ascii_letters:
+        for letter in ascii_lowercase:
             packages = Package.objects.filter(title__startswith=letter)
             if packages.count():
                 print >> stdout, '\nUpdating packages starting with the letter "%s"' % letter   
                 for package in Package.objects.filter(title__startswith=letter):
                     try:
                         package.save()
+                        count += 1
                     except RuntimeError:
-                        raise 'Too many requests issued to Github'
-                    print >> stdout, 'Successfully updated package "%s"' % package.title
+                        raise CommandError('Too many requests issued to Github threw a RuntimeError')
+                    print >> stdout, '---Successfully updated package "%s"' % package.title
+                    
+        print >> stdout, '\n%s packages updated' % count
 
 
 """
