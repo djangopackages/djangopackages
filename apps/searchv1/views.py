a41ff3273d2f9a1dd0e5a75a909dce79094360be
pydanny <pydanny@gmail.com>
2010-08-17 09:14:37 -0500

    New search autocomplete on home page

--- apps/searchv1/views.py
+++ apps/searchv1/views.py
@@ -1,4 +1,7 @@
+import simplejson
+
 from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext 
 
@@ -7,6 +10,31 @@ from package.models import Package
 
 from searchv1.forms import SearchForm
 
+def find_packages_autocomplete(q):
+    django_dash = 'django-%s' % q
+    django_space = 'django %s' % q    
+    return Package.objects.filter(
+                Q(title__istartswith=q) | 
+                Q(title__istartswith=django_dash) |
+                Q(title__istartswith=django_space))
+
+def find_grids_autocomplete(q):
+    return Grid.objects.filter(title__istartswith=q)
+
+def search_by_function_autocomplete(request, search_function):
+    """
+    Searches in Grids and Packages
+    """
+    q = request.GET.get('term', '')
+    form = SearchForm(request.GET or None)  
+    if q:
+        objects    = search_function(q)
+        objects    = objects.values_list('title', flat=True)    
+        json_response = simplejson.dumps(list(objects))
+    else:
+        json_response = simplejson.dumps([])
+
+    return HttpResponse(json_response, mimetype='text/javascript')
 
 def search(request, template_name='searchv1/search.html'):
     """
