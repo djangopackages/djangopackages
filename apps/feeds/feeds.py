1f76cf397838a12baa323a3cb855251fa8dd8bff
pydanny <pydanny@gmail.com>
2010-08-28 08:05:24 -0500

    Adding atom feed too

--- apps/feeds/feeds.py
+++ apps/feeds/feeds.py
@@ -1,16 +1,24 @@
 from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Atom1Feed
 from package.models import Package
 
-class LatestPackagesFeed(Feed):
+class RssLatestPackagesFeed(Feed):
     title = "Latest Django packages added"
     link = "/packages/latest"
     description = "The last 15 packages added"
 
     def items(self):
-        return Package.objects.all()[:15]
+        return Package.objects.all().order_by("-created")[:15]
 
     def item_title(self, item):
         return item.title
 
     def item_description(self, item):
         return item.repo_description
+        
+    def item_pubdate(self, item):
+        return item.created
+        
+class AtomLatestPackagesFeed(RssLatestPackagesFeed):
+    feed_type = Atom1Feed
+    subtitle = RssLatestPackagesFeed.description
--- apps/feeds/urls.py
+++ apps/feeds/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 
-from feeds import LatestPackagesFeed
+from feeds import *
 
 urlpatterns = patterns("",
-    (r'^packages/latest/rss/$', LatestPackagesFeed()),
+    (r'^packages/latest/rss/$', RssLatestPackagesFeed()),
+    (r'^packages/latest/atom/$', AtomLatestPackagesFeed()),    
 )
