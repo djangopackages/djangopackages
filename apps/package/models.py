14b5acbe186d9793262cad62d68a712314ff794a
pydanny <pydanny@gmail.com>
2010-08-28 07:56:42 -0500

    Package permalink to support feed

--- apps/package/models.py
+++ apps/package/models.py
@@ -9,6 +9,7 @@ from urllib import urlopen
 
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -185,6 +186,11 @@ class Package(BaseModel):
     def __unicode__(self):
         
         return self.title
+        
+    @models.permalink
+    def get_absolute_url(self):
+        return ("package", [self.slug])
+        
 
 class PackageExample(BaseModel):
     
