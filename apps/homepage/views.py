4f96c3c64b3ddadb07100a67573c5cb8468c0002
Taavi <taavi@taijala.com>
2010-09-11 04:32:41 +0800

    Improve the homepage to use fewer queries.
    Added annotations for categories in order to count packages all at once, instead of on a per-category basis. Added annotations for packages to count users all at once, instead of having to use a query for each package.

--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404 
 from django.template import RequestContext 
@@ -8,14 +9,14 @@ from homepage.models import Dpotw, Gotw, Tab
 def homepage(request, template_name="homepage.html"):
     
     categories = []
-    for category in Category.objects.all():
+    for category in Category.objects.annotate(package_count=Count("package")):
         element = {
             'title':category.title,
             'description':category.description,
-            'count': category.package_set.count(),
-            'slug':category.slug,
-            'title_plural':category.title_plural,            
-            'packages':category.package_set.order_by('-pypi_downloads', '-repo_watchers', 'title')[:9]
+            'count': category.package_count,
+            'slug': category.slug,
+            'title_plural': category.title_plural,            
+            'packages': category.package_set.annotate(usage_count=Count("usage")).order_by('-pypi_downloads', '-repo_watchers', 'title')[:9]
         }
         categories.append(element)
     
