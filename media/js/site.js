39ccff86e118adbc5094ac783ddf6753a8134a33
James Punteney <punteney@gmail.com>
2010-09-11 09:14:45 +0800

    Updating js for redirect and to support not making any changes

--- media/js/site.js
+++ media/js/site.js
@@ -5,12 +5,19 @@ $(".usage-button a").live('click', function(e) {
     e.target.href = ''; // Disabling the link to hopefully avoid double clicks
     
     $.getJSON(url, function(data) {
-        container.html(data.body); // Sets the button to be the rendered template
-        // Looking to update the count, the usage count needs to be within the usage-container
-        var count_el = container.parent('.usage-container').children('.usage-count');
-        if (count_el) {
-            count_el.html(parseInt(count_el.html(), 10)+data.change);
+        if (data.success === true) {
+            container.html(data.body); // Sets the button to be the rendered template
+            // Looking to update the count, the usage count needs to be within the usage-container
+            var count_el = container.parent('.usage-container').children('.usage-count');
+            if (count_el) {
+                count_el.html(parseInt(count_el.html(), 10)+data.change);
+            }            
+        } else {
+            if (data.redirect) {
+                window.location = data.redirect;
+            }
         }
     });
     e.preventDefault();
 });
+
