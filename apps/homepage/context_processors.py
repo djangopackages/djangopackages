f75a81cfe4baf3c6e48500d88fb1f040f301a361
Taavi <taavi@taijala.com>
2010-09-11 14:00:09 -0700

    Improved login functionality to always redirect a user back to the page they were viewing before they logged in, with the sole exception of never forwarding them back to the log out page!

--- apps/homepage/context_processors.py
+++ apps/homepage/context_processors.py
@@ -1,7 +1,21 @@
+from django.core.urlresolvers import reverse
+
 from homepage.models import Tab
 
 def grid_tabs(request):
     context = {}
     # TODO: Cache theses...
     context['grid_tabs'] = Tab.objects.all().select_related('grid')
+    return context
+
+def current_path(request):
+    """Adds the path of the current page to template context, but only
+    if it's not the path to the logout page. This allows us to redirect
+    user's back to the page they were viewing before they logged in,
+    while making sure we never redirect them back to the logout page!
+    
+    """
+    context = {}
+    if request.path != reverse('acct_logout'):
+        context['current_path'] = request.path
     return context
