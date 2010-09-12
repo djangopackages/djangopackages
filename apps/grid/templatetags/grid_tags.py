21561abeb60e6df1e0d17826f0778437d379a44f
Jonas Obrist <jonas.obrist@divio.ch>
2010-09-12 14:33:06 +0800

    Added tests for the style_element template filter Fixed +3 bug in style_element Added ability to prefix text with +/- icons

--- apps/grid/templatetags/grid_tags.py
+++ apps/grid/templatetags/grid_tags.py
@@ -1,27 +1,30 @@
-import re
-
 from django import template
 from django.conf import settings
 from django.template.defaultfilters import escape
-
 from grid.models import Element
+import re
+
+
 
 register = template.Library()
 
 static_url = settings.STATIC_URL
 
-plus_two_re = re.compile(r'(^\+2$|\+{2})')
-minus_two_re = re.compile(r'(^\-2$|\-{2})')
+plus_two_re = re.compile(r'^(\+2|\+{2})$')
+minus_two_re = re.compile(r'^(\-2|\-{2})$')
 
 plus_three_re = re.compile(r'^(\+[3-9]{1,}|\+{3,}|\+[1-9][0-9]+)$')
 minus_three_re = re.compile(r'^(\-[3-9]{1,}|\-{3,}|\-[1-9][0-9]+)$')
 
+YES = ('check', 'yes', 'good', '+1', '+')
+NO = ('bad', 'negative', 'evil', 'sucks', 'no', '-1', '-')
+
 @register.filter
 def style_element(text):
     low_text = text.strip().lower()
-    if low_text in ('check', 'yes', 'good', '+1', '+'):
+    if low_text in YES:
         return '<img src="%simg/icon-yes.gif" />' % settings.STATIC_URL
-    if low_text in ('bad', 'negative', 'evil', 'sucks', 'no', '-1', '-'):
+    if low_text in NO:
         return '<img src="%simg/icon-no.gif" />' % settings.STATIC_URL
 
     if plus_two_re.search(low_text):
@@ -35,11 +38,21 @@ def style_element(text):
 
     if minus_three_re.search(low_text):
         return '<img src="%simg/icon-no.gif" />' % settings.STATIC_URL * 3
-
-    # TODO Find the Django method that does this for me.
-    text = text.replace('&','&amp;')
-    text = text.replace('<','&lt;').replace('>',' &gt;')
-    text = text.replace("'",'&#39;').replace('"','&quot;')    
+    
+    text = escape(text)
+    
+    found = False
+    for positive in YES:
+        if text.startswith(positive):
+            text = '<img src="%simg/icon-yes.gif" />%s' % (settings.STATIC_URL,
+                                                           text[1:])
+            found = True
+            break
+    if not found:
+        for negative in NO:
+            if text.startswith(negative):
+                text = '<img src="%simg/icon-no.gif" />%s' % (settings.STATIC_URL,
+                                                              text[1:])
 
     
     return text
--- apps/grid/tests.py
+++ apps/grid/tests.py
@@ -5,19 +5,51 @@ unittest). These will both pass when you run "manage.py test".
 Replace these with more appropriate tests for your application.
 """
 
+from django.conf import settings
 from django.test import TestCase
+from grid.templatetags.grid_tags import style_element, YES, NO
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+class GridTest(TestCase):
+    def test_01_style_element_filter(self):
+        tests = [
+            ('+', 1, 0, ''),
+            ('++', 2, 0, ''),
+            ('+++', 3, 0, ''),
+            ('+1', 1, 0, ''),
+            ('+2', 2, 0, ''),
+            ('+3', 3, 0, ''),
+            ('+4', 3, 0, ''),
+            ('+42', 3, 0, ''),
+            ('-', 0, 1, ''),
+            ('--', 0, 2, ''),
+            ('---', 0, 3, ''),
+            ('-1', 0, 1, ''),
+            ('-2', 0, 2, ''),
+            ('-3', 0, 3, ''),
+            ('-4', 0, 3, ''),
+            ('-42', 0, 3, ''),
+            ('+test', 1, 0, 'test'),
+            ('-test', 0, 1, 'test'),
+        ]
+        for positive in YES:
+            tests.append((positive, 1, 0, ''))
+        for negative in NO:
+            tests.append((negative, 0, 1, ''))
+        for text, yes, no, endswith in tests:
+            output = style_element(text)
+            got_yes = output.count('<img src="%simg/icon-yes.gif" />' % settings.STATIC_URL)
+            self.assertEqual(
+                got_yes,
+                yes,
+                "%s resulted in %s yes-gifs instead of %s." % (text, got_yes, yes)
+            )
+            got_no = output.count('<img src="%simg/icon-no.gif" />' % settings.STATIC_URL)
+            self.assertEqual(
+                got_no,
+                no,
+                "%s resulted in %s no-gifs instead of %s." % (text, got_no, no)
+            )
+            self.assertTrue(
+                output.endswith(endswith),
+                "Expected %s to end with %s, got %s instead." % (text, endswith, output)
+            )
