e83265ebed952626898366e42e7f240f4c257690
Taavi <taavi@taijala.com>
2010-09-12 13:31:25 +0800

    Added caching (and the corresponding invalidation) for dealing with the list of a user's used packages.

--- apps/package/context_processors.py
+++ apps/package/context_processors.py
@@ -1,6 +1,12 @@
+from django.core.cache import cache
+
 def used_packages_list(request):
     context = {}
-    # TODO: Cache this result...
     if request.user.is_authenticated():
-        context['used_packages_list'] = request.user.package_set.values_list("pk", flat=True)
+        cache_key = "sitewide:used_packages_list:%s" % request.user.pk
+        used_packages_list = cache.get(cache_key)
+        if used_packages_list is None:
+            used_packages_list = request.user.package_set.values_list("pk", flat=True)
+            cache.set(cache_key, used_packages_list, 60 * 60 * 24)
+        context['used_packages_list'] = used_packages_list
     return context
