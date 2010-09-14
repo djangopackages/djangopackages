fab427dc494b0eeeb59c63887fd7d9fbcd2a046b
Taavi <taavi@taijala.com>
2010-09-13 20:43:57 -0700

    Accidentally removed one too many imports from the homepage tests, thus disabling them. Now fixed.

--- apps/homepage/tests/test_views.py
+++ apps/homepage/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.test import TestCase
 
 class FunctionalHomepageTest(TestCase):
     fixtures = ['test_initial_data.json']
