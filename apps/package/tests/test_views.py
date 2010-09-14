a7b9f85205da947e08f1d22c798688de68c1006d
Taavi <taavi@taijala.com>
2010-09-13 20:45:56 -0700

    Removed an another unnecessary import from the package test file. And yes, I'm sure this one is unnecessary. I tested it... with tests.

--- apps/package/tests/test_views.py
+++ apps/package/tests/test_views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.client import Client
 
 class FunctionalPackageTest(TestCase):
     fixtures = ['test_initial_data.json']
