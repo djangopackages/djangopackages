3a755ff4db746fa4465fa0fb966f697565070786
pydanny <pydanny@gmail.com>
2010-09-29 10:51:06 -0500

    Getting the user's list of packages to show up

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
 from idios.models import ProfileBase
+
+from package.models import Package
+from package.models import Repo
+
 
 
 class Profile(ProfileBase):
@@ -13,3 +18,21 @@ class Profile(ProfileBase):
         permissions = (
             ("is_moderator", "is_moderator"),
         )
+        
+    def my_packages(self):
+        # Move these bits into the Repo model
+        name = self.bitbucket_url
+        bitbucket_regex = r'^%s,|,%s,|%s$' % (name, name, name)
+        bitbucket_repo = Repo.objects.get(title = 'BitBucket')
+        
+        name = self.github_url
+        github_regex = r'^%s,|,%s,|%s$' % (name, name, name)
+        github_repo = Repo.objects.get(title = 'Github')
+
+        
+        return Package.objects.filter(
+                (Q(participants__regex=bitbucket_regex) & Q(repo=bitbucket_repo)) |
+                (Q(participants__regex=github_regex) & Q(repo=github_repo))                
+        )
+        
+        
