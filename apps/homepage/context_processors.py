6b9a8c3cefce5e2007a82f1d9ddcaca4add0dc2c
pydanny <pydanny@gmail.com>
2011-01-08 15:47:55 -0800

    Redoing the top grids so they work on all browsers

--- apps/homepage/context_processors.py
+++ apps/homepage/context_processors.py
@@ -1,13 +1,20 @@
+from itertools import izip, chain, repeat
+
 from django.core.urlresolvers import reverse
 from django.core.cache import cache
 
 from homepage.models import Tab
 
+def grouper(n, iterable, padvalue=None):
+    "grouper(3, 'abcdefg', 'x') --> ('a','b','c'), ('d','e','f'), ('g','x','x')"
+    return izip(*[chain(iterable, repeat(padvalue, n-1))]*n)
+
 def grid_tabs(request):
     cache_key = 'sitewide_grid_tabs'
     grid_tabs = cache.get(cache_key)
     if grid_tabs is None:
-        grid_tabs = Tab.objects.all().select_related('grid')
+        grid_tabs = list(Tab.objects.all().select_related('grid'))
+        grid_tabs = grouper(7, grid_tabs)
         cache.set(cache_key, grid_tabs, 60 * 5)
     return {'grid_tabs': grid_tabs}
 
