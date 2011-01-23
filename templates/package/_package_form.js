e6f649a4062a12f75e6f06079a4dea6f73470946
pydanny <pydanny@gmail.com>
2011-01-22 17:21:20 -0800

    Increased usability on the package add/edit form

--- templates/package/_package_form.js
+++ templates/package/_package_form.js
@@ -1,6 +1,6 @@
 // need to add 'is_other' to repos so we can make this work
 
-String.prototype.startsWith = function(str){
+String.prototype.starts_with = function(str){
     return (this.indexOf(str) === 0);
 }
 
@@ -25,12 +25,37 @@ $("#id_repo_url").keyup(function(e) {
 $("#id_repo_url").change(function(e) {
  
     $("#target").text($("#id_repo_url").val());      
-
-    $.each(repo_urls, function(key, value) {         
     
-        var url = $("#id_repo_url").val();
-        var url_array = url.split('/');
-        if (url.startsWith(key)){
+    var url = $("#id_repo_url").val();
+    // this fixes the problem with trailing slashes
+    while (1==1){
+            if (url.slice(url.length-1) === '/'){
+                url = url.slice(0, url.length-1);
+                $("#id_repo_url").val(url);
+                }
+            else {
+                break;
+            };
+        };
+    // for github
+    if (url.starts_with('http://github.com')){
+        url = url.replace('http://github.com','https://github.com');
+        $("#id_repo_url").val(url);
+    };
+    if (url.starts_with('git@github.com:')){
+        url = url.replace("git@github.com:","https://github.com/");
+        url = url.slice(0, url.length-4);
+        $("#id_repo_url").val(url);        
+    };
+    // for bitbucket
+    if (url.starts_with('http://bitbucket.org')){
+        url = url.replace('http://bitbucket.org','https://bitbucket.org');
+        $("#id_repo_url").val(url);        
+    };
+    
+    var url_array = url.split('/');
+    $.each(repo_urls, function(key, value) {
+        if (url.starts_with(key)){
             if (url !== key){
                 $("#id_repo").val(value);
                 if ($("#id_title").val().length === 0) {
