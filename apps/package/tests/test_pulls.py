6ad125a64a5d4511ef05e7b9939a92058ced6150
James Pacileo <jamespacileo@gmail.com>
2011-03-15 14:18:57 -0700

    Test file work

--- apps/package/tests/test_pulls.py
+++ apps/package/tests/test_pulls.py
@@ -1,3 +1,14 @@
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+
+from apps.package.models import Package, Repo
+
+
+
+class GitHubTest(TestCase):
+
+    fixtures = ['test_initial_data.json']
+
+    def test_repos(self):
+        print Repo.objects.all()
