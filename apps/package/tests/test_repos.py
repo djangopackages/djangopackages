d694da0d744b48db1818c96def774e6ac6c88ca1
Szilveszter Farkas <szilveszter.farkas@gmail.com>
2011-03-15 16:46:10 -0400

    Added fetch_commits test, and separated Launchpad tests.

--- apps/package/tests/test_repos.py
+++ apps/package/tests/test_repos.py
@@ -1,7 +1,28 @@
 from django.test import TestCase
 
 from package.repos.launchpad import repo_handler as launchpad_handler
-from package.models import Package
+from package.models import Commit, Package
+
+
+class TestLaunchpadRepo(TestCase):
+    def setUp(self):
+        self.package = Package.objects.create(
+            title="Django-PreFlight",
+            slug="django-preflight",
+            repo_url="https://code.launchpad.net/~canonical-isd-hackers/django-preflight/trunk")
+    
+    def test_fetch_commits(self):
+        # TODO: mock these so no network access is required
+        self.assertEqual(Commit.objects.count(), 0)
+        launchpad_handler.fetch_commits(self.package)
+        self.assertNotEqual(Commit.objects.count(), 0)
+
+    def test_fetch_metadata(self):
+        # TODO: mock these so no network access is required
+        p_ret = launchpad_handler.fetch_metadata(self.package)
+        self.assertTrue(p_ret.repo_watchers > 0)
+        self.assertTrue(p_ret.repo_forks > 0)
+        self.assertEqual(p_ret.participants, 'canonical-isd-hackers')
 
 
 class TestRepoHandlers(TestCase):
@@ -38,14 +59,3 @@ class TestRepoHandlers(TestCase):
         self.assertEqual(p.repo_description, "")
         self.assertEqual(p.repo_watchers, 0)
         p.fetch_commits()
-
-    def test_launchpad_pull(self):
-        # TODO: mock these so no network access is required
-        p = Package(
-            title="Django-PreFlight",
-            slug="django-preflight",
-            repo_url="https://code.launchpad.net/~canonical-isd-hackers/django-preflight/trunk")
-        p_ret = launchpad_handler.fetch_metadata(p)
-        self.assertTrue(p_ret.repo_watchers > 0)
-        self.assertTrue(p_ret.repo_forks > 0)
-        self.assertEqual(p_ret.participants, 'canonical-isd-hackers')
