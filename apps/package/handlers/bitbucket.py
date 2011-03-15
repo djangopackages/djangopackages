55ed8389eeef291a32d1da9e28252b9af16c5095
Szilveszter Farkas <szilveszter.farkas@gmail.com>
2011-03-15 14:40:49 -0400

    Refactored the Bitbucket handler to use BaseHandler.

--- apps/package/handlers/bitbucket.py
+++ apps/package/handlers/bitbucket.py
@@ -7,52 +7,57 @@ try:
 except ImportError:
     import json
 
-from package.utils import uniquer
+from package.handlers.base_handler import BaseHandler
 
 API_TARGET = "https://api.bitbucket.org/1.0/repositories/"
 
 descendants_re = re.compile(r"Forks/Queues \((?P<descendants>\d+)\)")
 
 
-def pull(package):
-    
-    # prep the target name
-    repo_name = package.repo_name()
-    target = API_TARGET + repo_name
-    if not target.endswith("/"):
-        target += "/"
-    
-    # open the target and read the content
-    response = urlopen(target)
-    response = response.read()
-    
-    # dejsonify the results
-    try:
-        data = json.loads(response)
-    except json.decoder.JSONDecodeError:
-        # TODO - log this better
-        message = "%s had a JSONDecodeError during bitbucket.repo.pull" % (package.title)
-        warn(message)
-        return package
+class BitbucketHandler(BaseHandler):
+    title = 'Bitbucket'
+    url = 'https://bitbucket.org'
+    repo_regex = r'https://bitbucket.org/[\w\-\_]+/([\w\-\_]+)/{0,1}'
+    slug_regex = r'https://bitbucket.org/[\w\-\_]+/([\w\-\_]+)/{0,1}'
 
-    package.repo_watchers    = data.get("followers_count",0)
-    package.repo_description = data.get("description","")
-    
-    # screen scrape to get the repo_forks off of bitbucket HTML pages
-    target = package.repo_url
-    if not target.endswith("/"):
-        target += "/"
-    target += "descendants"
-    html = urlopen(target)
-    html = html.read()
-    try:
-        package.repo_forks = descendants_re.search(html).group("descendants")
-    except AttributeError:
-        package.repo_forks = 0
-    
-    try:
-        package.participants = package.repo_url.split("/")[3] # the only way known to fetch this from bitbucket!!!
-    except IndexError:
-        package.participants = ""
+    def pull(self, package):
+        # prep the target name
+        repo_name = package.repo_name()
+        target = API_TARGET + repo_name
+        if not target.endswith("/"):
+            target += "/"
         
-    return package
+        # open the target and read the content
+        response = urlopen(target)
+        response = response.read()
+        
+        # dejsonify the results
+        try:
+            data = json.loads(response)
+        except json.decoder.JSONDecodeError:
+            # TODO - log this better
+            message = "%s had a JSONDecodeError during bitbucket.repo.pull" % (package.title)
+            warn(message)
+            return package
+
+        package.repo_watchers    = data.get("followers_count",0)
+        package.repo_description = data.get("description","")
+        
+        # screen scrape to get the repo_forks off of bitbucket HTML pages
+        target = package.repo_url
+        if not target.endswith("/"):
+            target += "/"
+        target += "descendants"
+        html = urlopen(target)
+        html = html.read()
+        try:
+            package.repo_forks = descendants_re.search(html).group("descendants")
+        except AttributeError:
+            package.repo_forks = 0
+        
+        try:
+            package.participants = package.repo_url.split("/")[3] # the only way known to fetch this from bitbucket!!!
+        except IndexError:
+            package.participants = ""
+            
+        return package
