03be191b2eba166302b9133e76745fced058b6a6
ptone <preston@ptone.com>
2011-03-16 12:32:22 -0700

    added test for version ordering in Version manager
    unfortunately doesn't look like there is a way to
    preserve the type as a QS, can only return a
    sorted list.

--- apps/package/tests/__init__.py
+++ apps/package/tests/__init__.py
@@ -3,3 +3,4 @@ from package.tests.test_repos import *
 from package.tests.test_sourceforge import *
 from package.tests.test_utils import *
 from package.tests.test_views import *
+from package.tests.test_models import *
