dcf3be326f9431064951b95d89974080cf3874c0
Vasja Volin <vasja@superprofundo.com>
2011-03-16 16:34:55 -0400

    Removed test_pulls from test suite, fixed minor bug in bitbucket repo handler

--- apps/package/tests/__init__.py
+++ apps/package/tests/__init__.py
@@ -1,4 +1,4 @@
-from package.tests.test_pulls import *
+#from package.tests.test_pulls import *
 from package.tests.test_repos import *
 #from package.tests.test_sourceforge import *
 from package.tests.test_utils import *
