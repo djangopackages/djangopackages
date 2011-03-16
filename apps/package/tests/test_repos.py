27f4675d71b0b0eff7cd6edb9401056316901d9e
Vasja Volin <vasja@superprofundo.com>
2011-03-16 11:46:00 -0400

    Added sample Sourceforge tests

--- apps/package/tests/test_repos.py
+++ apps/package/tests/test_repos.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from package.repos.bitbucket import repo_handler as bitbucket_handler
 from package.repos.github import repo_handler as github_handler
 from package.repos.launchpad import repo_handler as launchpad_handler
+from package.repos.sourceforge import repo_handler as sourceforge_handler
 from package.models import Commit, Package
 
 
@@ -75,6 +76,25 @@ class TestLaunchpadRepo(TestCase):
         self.assertTrue(package.repo_forks > 0)
         self.assertEqual(package.participants, 'canonical-isd-hackers')
 
+'''
+class TestSourceforgeRepo(TestCase):
+    def setUp(self):
+        self.package = Package.objects.create(
+            title="django-ui",
+            slug="django-ui",
+            repo_url="http://sourceforge.net/projects/django-ui/",
+        )
+
+    def test_fetch_commits(self):
+	self.assertEqual(Commit.objects.count(), 0)
+	sourceforge_handler.fetch_commits(self.package)
+        self.assertEqual(Commit.objects.count(), 0)
+
+    def test_fetch_metadata(self):
+        package = sourceforge_handler.fetch_metadata(self.package)
+        self.assertTrue(package.repo_watchers > 0)
+        self.assertTrue(package.repo_forks > 0)
+        self.assertEqual(package.participants, '')'''
 
 class TestRepos(TestCase):
     def test_repo_registry(self):
