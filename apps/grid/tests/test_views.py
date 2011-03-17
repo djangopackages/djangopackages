03ea2513bf7504ea7bc1eb580e9543433b5e7d90
Aaron Kavlie <akavlie@gmail.com>
2011-03-17 01:07:30 -0400

    More test coverage for grid views.

--- apps/grid/tests/test_views.py
+++ apps/grid/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 from grid.models import Grid, Element, Feature, GridPackage
+from package.models import Package
 
 class FunctionalGridTest(TestCase):
     fixtures = ['test_initial_data.json']
@@ -150,6 +151,7 @@ class FunctionalGridTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
+
     def test_add_gridpackage_view(self):
         url = reverse('add_grid_package', kwargs={'grid_slug': 'testing'})
         response = self.client.get(url)
@@ -162,6 +164,47 @@ class FunctionalGridTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'grid/add_grid_package.html')
+
+        # Test form post
+        #count = GridPackage.objects.count()
+        #response = self.client.post(url, {
+            #'package': 'TEST NAME',
+        #}, follow=True)
+        #self.assertEqual(GridPackage.objects.count(), count + 1)
+        #self.assertContains(response, 'TEST TITLE')
+
+
+    def test_add_new_grid_package_view(self):
+        url = reverse('add_new_grid_package', kwargs={'grid_slug': 'testing'})
+        response = self.client.get(url)
+
+        # The response should be a redirect, since the user is not logged in.
+        self.assertEqual(response.status_code, 302)
+        
+        # Once we log in the user, we should get back the appropriate response.
+        self.assertTrue(self.client.login(username='user', password='user'))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'package/package_form.html')
+
+        # Test form post
+        count = Package.objects.count()
+        response = self.client.post(url, {
+            'repo_url': 'http://www.example.com',
+            'title': 'Test package',
+            'slug': 'test-package',
+            'pypi_url': 'http://pypi.python.org/pypi/mogo/0.1.1',
+            'category': 1 
+        }, follow=True)
+        self.assertEqual(Package.objects.count(), count + 1)
+        self.assertContains(response, 'Test package')
+
+
+    #def test_ajax_grid_list_view(self):
+        #url = reverse('ajax_grid_list') + '?q=django&package_id=1' 
+        #response = self.client.get(url)
+        #self.assertContains(grid, 'django-cms')
+
 
     def test_delete_gridpackage_view(self):
         count = GridPackage.objects.count()
