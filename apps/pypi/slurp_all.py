9f15a79a012b884595d8048876ca8c7af79f30f0
pydanny <pydanny@gmail.com>
2011-03-19 15:34:12 -0700

    get_or_create package stuff

--- apps/pypi/slurp_all.py
+++ apps/pypi/slurp_all.py
@@ -11,16 +11,41 @@ import xmlrpclib
 
 from django.template.defaultfilters import slugify
 
-from package.models import Package, Version
+from package.models import Category, Package, Version
 from pypi.models import PypiUpdateLog
 
-PYPI = xmlrpclib.Server('http://pypi.python.org/pypi')
+base_url = "http://pypi.python.org/pypi/"
+PYPI = xmlrpclib.Server(base_url)
 
 class Slurper(object):
     
     def __init__(self):
         self.package_names = PYPI.list_packages()
+        self.dumb_category, created = Category.objects.get_or_create(
+                                title='dummy', slug='dummy')
+        self.dumb_category.save()
+        
+    def get_latest_version(self, package_name):
+        """ Make this actually work"""
+        return PYPI.package_releases(package_name)[0]
+        
+    def get_or_create_package(self, package_name):
+        version = self.get_latest_version(package_name)
+        data = PYPI.release_data(package_name, version)
 
-    
-    
-    
+        package, created = Package.objects.get_or_create(
+            title           = data['name'],
+            slug            = slugify(data['name']),
+            category        = self.dumb_category,
+            pypi_url        = base_url + data['name']
+        )
+        package.repo_description = data['summary'] or data['description']
+        package.save()
+        return package
+        
+    def get_or_create_all_packages(self):
+
+        #for package_name in self.package_names:        
+        for package_name in ['Django']:
+            package = self.get_or_create_package(package_name)
+            break
