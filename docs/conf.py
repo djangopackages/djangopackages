14ddb24429633579c7273d12cd70fb0572662cc8
pydanny <pydanny@gmail.com>
2011-03-20 13:14:58 -0700

    Trying out the virtualenv action on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -21,15 +21,13 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-# TODO - talk to @ericholscher about trying to get this to work.
-if False:
-    sys.path.insert(0, os.path.abspath('../../'))
-    sys.path.insert(0, os.path.abspath('../apps/'))
-    sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../apps/'))
+sys.path.insert(0, os.path.abspath('..'))
 
-    import settings
-    from django.core.management import setup_environ
-    setup_environ(settings)
+import settings
+from django.core.management import setup_environ
+setup_environ(settings)
 
 # -- General configuration -----------------------------------------------------
 
