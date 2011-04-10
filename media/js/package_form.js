0de19077cb848888965a9044be82aca59b9d9064
Jonas Obrist <jonas.obrist@divio.ch>
2011-04-10 23:52:07 +0200

    Forgot that I renamed a var in JS

--- media/js/package_form.js
+++ media/js/package_form.js
@@ -1,21 +1,18 @@
-// need to add 'is_other' to repos so we can make this work
-	
-var get_slug_from_repo_data = function (repo_root_url, repo_branch_url) {
-	var slug = null;
-	for (i=0;i<repo_data.length;i++) {
-		if (repo_data[i].url == repo_root_url) {
-			var regexp_title = new RegExp(repo_data[i].repo_regex);
-			var match_obj = regexp_title.exec(repo_branch_url);
-			if (match_obj !== null) {
-				slug = match_obj[1]
-			}
-			break;
-		}
-	}
-	return slug;
-}	
-
 function package_form(data){
+	var get_slug_from_repo_data = function (repo_root_url, repo_branch_url) {
+		var slug = null;
+		for (i=0;i<data.length;i++) {
+			if (data[i].url == repo_root_url) {
+				var regexp_title = new RegExp(data[i].repo_regex);
+				var match_obj = regexp_title.exec(repo_branch_url);
+				if (match_obj !== null) {
+					slug = match_obj[1]
+				}
+				break;
+			}
+		}
+		return slug;
+	}
 	var REPLACEMENTS = {
 		'https://github.com': [
 			'http://github.com',
@@ -87,7 +84,7 @@ function package_form(data){
 		repo_url.val(url);
 		
 		var url_array = url.split('/');
-		$.each(repo_data, function(index, item) {
+		$.each(data, function(index, item) {
 			if (url.starts_with(item.url)){
 				if (url !== item.url){
 					var slug = null;
