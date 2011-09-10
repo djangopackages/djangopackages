7520851bc15b646058ee10a37724b00ef11a59e4
Taylor Mitchell <taylor.mitchell@gmail.com>
2011-09-10 15:50:23 -0700

    Add configuration for gondor

--- opencomparison/settings/production.py
+++ opencomparison/settings/production.py
@@ -4,9 +4,16 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 SERVE_MEDIA = False
 
-# local_settings.py can be used to override environment-specific settings
-# like database and email that differ between development and production.
-try:
-    from local_settings import *
-except ImportError:
-    pass
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'djangopackages-noreply@djangopackages.com'
+EMAIL_USE_TLS = True
+
+# no try/except here since these not being populated would be bad
+
+# get DATABASES, GONDOR_REDIS_HOST, GONDOR_REDIS_PORT, GONDOR_REDIS_PASSWORD
+from local_settings import *
+
+# get EMAIL_HOST_PASSWORD, SECRET_KEY
+from .secret import *
+
