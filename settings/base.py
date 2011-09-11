145e9660af5a072a7ca2f04b8fca9e73fc09ddca
Taylor Mitchell <taylor.mitchell@gmail.com>
2011-09-10 17:22:18 -0700

    Fix admin media to be a subdir of the STATIC_URL

--- settings/base.py
+++ settings/base.py
@@ -73,8 +73,7 @@ STATICFILES_DIRS = [
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-# ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
-ADMIN_MEDIA_PREFIX = "/site_media/admin/"
+ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
 
 # List of callables that know how to import templates from various sources.
 if DEBUG:
