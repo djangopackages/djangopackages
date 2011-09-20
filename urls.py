79c1482fef94403fc751671b452e10b78273ac00
Kenneth Love <kenneth@gigantuan.net>
2011-09-19 20:44:52 -0700

    updated django-social-auth in requirements/mkii.txt. added github backend to auth backends. added links to login/sign up pages. added pre_update signal for github accounts.

--- urls.py
+++ urls.py
@@ -12,48 +12,49 @@ from package.views import package_autocomplete, category, packaginate
 
 urlpatterns = patterns("",
 
+    url('', include('social_auth.urls')),
     url(r"^$", homepage, name="home"),
-    url(r"^accounts/", include("accounts.urls")),    
+    url(r"^accounts/", include("accounts.urls")),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^about/", include("about.urls")),
     url(r"^profiles/", include("profiles.urls")),
     url(r"^notices/", include("notification.urls")),
     url(r"^packages/", include("package.urls")),
-    url(r"^grids/", include("grid.urls")),  
+    url(r"^grids/", include("grid.urls")),
     url(r"^search/", include("searchv1.urls")),
     url(r"^feeds/", include("feeds.urls")),
-    
+
     url(r"^categories/(?P<slug>[-\w]+)/$", category, name="category"),
     url(r"^categories/$", homepage, name="categories"),
-    url(r"^packaginator/$", 
+    url(r"^packaginator/$",
                 direct_to_template,
-                {'template': 'package/packaginator.html'}, 
-                name="packaginator"), 
-                
-    url(r"^packaginate/$", 
+                {'template': 'package/packaginator.html'},
+                name="packaginator"),
+
+    url(r"^packaginate/$",
                 packaginate,
-                name="packaginate"),                   
-    
+                name="packaginate"),
+
     url(
         regex = '^autocomplete/package/$',
         view = package_autocomplete,
-        name    = 'package_autocomplete',        
+        name    = 'package_autocomplete',
     ),
 
     #TODO - fix these by using django-registration
-    
-    url(r"^email/$", direct_to_template, {"template": "about/about.html"}, name="acct_email"), 
+
+    url(r"^email/$", direct_to_template, {"template": "about/about.html"}, name="acct_email"),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html', }, 'login',),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {}, 'logout',),    
-    
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {}, 'logout',),
+
     # Built-in django auth views
      (r'^changepassword/$', 'django.contrib.auth.views.password_change', {}, 'change_password',),
      (r'^changepassword/done/$', 'django.contrib.auth.views.password_change_done', {}, 'change_password_done'),
      (r'^password_reset/$', 'django.contrib.auth.views.password_reset', {'is_admin_site': False},'acct_passwd_reset'),
      (r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
      (r'^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {}, 'password_reset_confirm'),
-     (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),    
-    
+     (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+
 )
 
 from apiv1.api import Api
@@ -74,7 +75,7 @@ v1_api.register(DpotwResource())
 v1_api.register(UserResource())
 
 urlpatterns += patterns('',
-    url(r"^api/", include(v1_api.urls)), 
+    url(r"^api/", include(v1_api.urls)),
 )
 
 
