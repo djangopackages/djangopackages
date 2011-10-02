7e33b7307355b65c69a7347e580651499e28e3da
pydanny <pydanny@gmail.com>
2011-10-02 00:19:37 -0700

    nuking weird export system

--- templates/core/export/package.category.py
+++
@@ -1,9 +0,0 @@
-{% for category in categories %}
-    category_{{ forloop.counter }} = Category.objects.create(
-        title = "{{ category.title }}"[:50],
-        slug  = "{{ category.slug }}",
-        description = "{{ category.description }}",
-        title_plural = "{{ category.title_plural }}"[:50],
-        show_pypi = {{ category.show_pypi }}
-    )
-{% endfor %}
