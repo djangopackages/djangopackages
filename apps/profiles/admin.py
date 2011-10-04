ddbaeeb9d576747cc34fa86e374988878965306a
pydanny <pydanny@gmail.com>
2011-10-04 15:01:44 -0700

    Profile fixes

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -4,9 +4,17 @@ from reversion.admin import VersionAdmin
 
 from profiles.models import Profile
 
+def username(obj):
+    return (obj.user.username)
+username.short_description="User username"
+
+def user_email(obj):
+    return (obj.user.email)
+username.short_description="User email"
+
 class ProfileAdmin(VersionAdmin):
     
     search_fields = ("user__username","github_account", "user__email", "email")
-    list_display = ("github_account", "email", "email")
+    list_display = ("github_account", "email", username, user_email)
 
 admin.site.register(Profile, ProfileAdmin)
