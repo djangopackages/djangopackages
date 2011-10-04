69db6264cd722e1aa7a2c588c99b39db265954d1
pydanny <pydanny@gmail.com>
2011-10-04 14:44:23 -0700

    Search on profiles

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -4,4 +4,9 @@ from reversion.admin import VersionAdmin
 
 from profiles.models import Profile
 
-admin.site.register(Profile, VersionAdmin)
+class ProfileAdmin(VersionAdmin):
+    
+    search_fields = ("user__username","github_account", "user__email", "email")
+    list_display = ("github_account", "email", "email")
+
+admin.site.register(Profile, ProfileAdmin)
