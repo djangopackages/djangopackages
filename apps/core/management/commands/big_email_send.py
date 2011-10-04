ac3fffbdec0560c7d752adadc5afeb88ce63bc78
pydanny <pydanny@gmail.com>
2011-10-03 17:51:44 -0700

    big email sender made

---
+++ apps/core/management/commands/big_email_send.py
@@ -0,0 +1,35 @@
+import time
+from sys import stdout
+
+from django.conf import settings
+from django.core.management.base import CommandError, NoArgsCommand
+from django.utils.importlib import import_module
+from django.utils.module_loading import module_has_submodule
+
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+
+
+class Command(NoArgsCommand):
+    
+    help = "Send out email to everyone"
+    
+    def handle(self, *args, **options): 
+        print >> stdout, "Commencing big email send"
+
+        #users = User.objects.filter(is_active=True).exclude(email__contains="qq.com").exclude(email__contains="tom.com")
+        users = User.objects.filter(username__in=("pydanny","audreyr"))
+
+        for index, user in enumerate(users):
+            
+            message = """
+"""            
+
+            send_mail(
+                subject="",
+                message=message,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[user.email,],
+            )            
+            print "Sent to", index, user.email
+            time.sleep(1)
