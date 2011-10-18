8167558b0d23a13517f071cd765270874ba72b12
pydanny <pydanny@gmail.com>
2011-10-18 07:20:34 -0700

    Removing deprecated templatetag

--- apps/profiles/templatetags/profile_tags.py
+++ apps/profiles/templatetags/profile_tags.py
@@ -5,8 +5,3 @@ register = template.Library()
 @register.filter
 def package_usage(user):
     return user.package_set.all()
-
-@register.simple_tag
-def user_display(user):
-	""" TODO - turn this into an inclusion tag """
-	return user
