9e4fcb6bc6f0b388e950527abf2f5011a85d8a9b
pydanny <pydanny@gmail.com>
2011-10-25 21:56:43 -0700

    cleaning imports

--- apps/package/views.py
+++ apps/package/views.py
@@ -12,7 +12,6 @@ from django.db.models import Q, Count, get_model
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseForbidden
 from django.shortcuts import render_to_response, get_object_or_404, render
 from django.template import RequestContext
-from django.template.loader import render_to_string
 
 from homepage.models import Dpotw, Gotw
 
