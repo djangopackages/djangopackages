bc7b3d61f83b39ec5047dacef0479c0dd46282bb
pydanny <pydanny@gmail.com>
2011-11-24 02:09:56 -0800

    last of render_to_response removals

--- apps/searchv2/views.py
+++ apps/searchv2/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.http import HttpResponseForbidden, HttpResponseRedirect, HttpResponse
-from django.shortcuts import render_to_response, render
+from django.shortcuts import render
 from django.template import RequestContext
 
 from package.models import Package
@@ -71,12 +71,10 @@ def search(request, template_name='searchv2/search.html'):
 
     form = SearchForm(request.GET or None)
 
-    return render_to_response(template_name, {
-        'items': search_function(q),
-        'form':form
-        },
-        context_instance=RequestContext(request)
-    )
+    return render(request. template_name, {
+            'items': search_function(q),
+            'form':form
+        })
     
 def search_packages_autocomplete(request):
     """
