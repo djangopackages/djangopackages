b598ac26cbe2400cbe1ff284cd7f0e59270e5fa9
pydanny <pydanny@gmail.com>
2011-11-25 02:44:44 -0800

    Pushing the auth on context processors into settings to be modernized

--- settings.py
+++ settings.py
@@ -108,6 +108,7 @@ TEMPLATE_DIRS = [
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = [
+    #"django.contrib.auth.context_processors.auth",
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
