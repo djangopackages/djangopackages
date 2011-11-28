bac6c128e23c65b7a2e055aebf9cbc2d657c03ec
pydanny <pydanny@gmail.com>
2011-11-28 00:48:29 -0800

    Removing unnecessary bits

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls.defaults import *
 from django.db.models import Count
-from django.views.generic.list_detail import object_detail, object_list
-from django.views.generic.date_based import archive_index
-from django.views.generic.simple import direct_to_template
 
 from profiles import views
-
 
 urlpatterns = patterns("",
 	url(r"^edit/$", views.profile_edit, name="profile_edit"),    
