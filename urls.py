7a199a9bb617d39bd84e96cccd975f9a7b7cf0a8
pydanny <pydanny@gmail.com>
2011-12-06 09:45:00 -0800

    footer link fix

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic.base import TemplateView
-from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
