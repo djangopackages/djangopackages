2905ee5e945be5435e984fea9c3d343e316f31db
Daniel Greenfeld <pydanny@gmail.com>
2012-03-13 18:02:08 -0700

    fix in urls

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic.base import TemplateView
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
