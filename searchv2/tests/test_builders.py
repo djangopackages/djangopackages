30da867d3b9dfcffbe2058ecf7acedb9629aa540
Daniel Greenfeld <pydanny@gmail.com>
2012-04-17 09:47:07 -0700

    Test tweak

--- searchv2/tests/test_builders.py
+++ searchv2/tests/test_builders.py
@@ -1,17 +1,14 @@
-from datetime import datetime
-
 from django.test import TestCase
 
-from package.models import Package
-from package.tests import data, initial_data
+from package.tests import initial_data
 from searchv2.models import SearchV2
 from searchv2.builders import build_1
 
-class BuilderTest(TestCase):  
-    
+class BuilderTest(TestCase):
+
     def setUp(self):
-        initial_data.load()    
-        
+        initial_data.load()
+
     def test_build_1_count(self):
         self.assertEquals(SearchV2.objects.count(), 0)
         build_1(False)
