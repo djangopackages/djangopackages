4fdb8bb7e322b846e9515f19b9b77adb9ef3de52
Daniel Greenfeld <pydanny@gmail.com>
2012-05-12 20:19:51 -0700

    no package updater

--- package/management/commands/package_updater.py
+++ package/management/commands/package_updater.py
@@ -1,11 +1,13 @@
+# python -m smtpd -n -c DebuggingServer localhost:1025
+
 from socket import error as socket_error
-from sys import stdout
 from time import sleep, gmtime, strftime
 from xml.parsers.expat import ExpatError
 from xmlrpclib import ProtocolError
 
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
+from django.core.mail import send_mail
 
 from package.models import Package
 
@@ -16,36 +18,50 @@ class Command(NoArgsCommand):
 
     def handle(self, *args, **options):
 
-        print >> stdout, "Commencing package updating now at %s " % strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+        text = "Commencing package updating now at %s " % strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 
         for index, package in enumerate(Package.objects.all()):
+            print index
+            if index > 3:
+                break
             try:
                 try:
                     package.fetch_metadata()
                     package.fetch_commits()
                 except socket_error, e:
-                    print >> stdout, "For '%s', threw a socket.error: %s" % (package.title, e)
+                    text += "\nFor '%s', threw a socket.error: %s" % (package.title, e)
+                    #print >> stdout, "For '%s', threw a socket.error: %s" % (package.title, e)
                     continue
             except RuntimeError, e:
-                message = "For '%s', too many requests issued to repo threw a RuntimeError: %s" % (package.title, e)
-                print >> stdout, message
+                #message = "For '%s', too many requests issued to repo threw a RuntimeError: %s" % (package.title, e)
+                text += "\nFor '%s', too many requests issued to repo threw a RuntimeError: %s" % (package.title, e)
                 continue
             except UnicodeDecodeError, e:
-                message = "For '%s', UnicodeDecodeError: %s" % (package.title, e)
-                print >> stdout, message
+                #message = "For '%s', UnicodeDecodeError: %s" % (package.title, e)
+                #print >> stdout, message
+                text += "\nFor '%s', UnicodeDecodeError: %s" % (package.title, e)
                 continue
             except ProtocolError, e:
-                message = "For '%s', xmlrpc.ProtocolError: %s" % (package.title, e)
-                print >> stdout, message
+                #message = "For '%s', xmlrpc.ProtocolError: %s" % (package.title, e)
+                #print >> stdout, message
+                text += "\nFor '%s', xmlrpc.ProtocolError: %s" % (package.title, e)
                 continue
             except ExpatError, e:
-                message = "For '%s', ExpatError: %s" % (package.title, e)
-                print >> stdout, message
+                #message = "For '%s', ExpatError: %s" % (package.title, e)
+                #print >> stdout, message
+                text += "\nFor '%s', ExpatError: %s" % (package.title, e)
                 continue
 
             if not hasattr(settings, "GITHUB_ACCOUNT"):
                 sleep(5)
-            print >> stdout, "%s. Successfully updated package '%s'" % (index + 1, package.title)
+            #print >> stdout, "%s. Successfully updated package '%s'" % (index + 1, package.title)
+            text += "\n%s. Successfully updated package '%s'" % (index + 1, package.title)
 
-        print >> stdout, "-" * 40
-        print >> stdout, "Finished at %s" % strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+        #print >> stdout, "-" * 40
+        text += "\n"
+        text += "-" * 40
+        #print >> stdout, "Finished at %s" % strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+        text += "\nFinished at %s" % strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+
+        print text
+
