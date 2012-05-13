267f0b2adb36b9187d8cfa3c85ae49d16dcdeb35
Randall Degges <rdegges@gmail.com>
2012-05-12 18:19:12 -0700

    Adding an EMAIL_PORT setting to settings.heroku.

--- settings/heroku.py
+++ settings/heroku.py
@@ -30,6 +30,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'your_email@example.com')
+EMAIL_PORT = environ.get('EMAIL_PORT', 587)
 EMAIL_SUBJECT_PREFIX = environ.get('EMAIL_SUBJECT_PREFIX', '[Django Packages] ')
 
 
