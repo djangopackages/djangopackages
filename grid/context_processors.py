a1ca9cba0e199a729f41a04bb29fea4a99fe8c3c
Daniel Greenfeld <pydanny@gmail.com>
2012-05-12 20:53:29 -0700

    Removing another cache call

--- grid/context_processors.py
+++ grid/context_processors.py
@@ -1,8 +1,5 @@
 from itertools import izip, chain, repeat
 
-from django.core.urlresolvers import reverse
-from django.core.cache import cache
-
 from grid.models import Grid
 
 
@@ -12,10 +9,6 @@ def grouper(n, iterable, padvalue=None):
 
 
 def grid_headers(request):
-    cache_key = 'grid_headers'
-    grid_headers = cache.get(cache_key)
-    if grid_headers is None:
-        grid_headers = list(Grid.objects.filter(header=True))
-        grid_headers = grouper(7, grid_headers)
-        cache.set(cache_key, grid_headers, 60 * 5)
+    grid_headers = list(Grid.objects.filter(header=True))
+    grid_headers = grouper(7, grid_headers)
     return {'grid_headers': grid_headers}
