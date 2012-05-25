16636be3c23faa74e9eff471f849cffd69339fa5
Daniel Greenfeld <pydanny@gmail.com>
2012-05-25 12:17:25 -0700

    Tweak rtd.js

--- static/js/rtd.js
+++ static/js/rtd.js
@@ -1,25 +1,25 @@
 $.fn.checkRTD = function(options){
-	var settings = $.extend( {
+    var settings = $.extend( {
       'yes': 'Yes',
       'no' : 'No'
     }, options);
-	return this.each(function(){
-		var ele = $(this);
-		var slug = ele.attr('rel');
-		var url = "http://readthedocs.org/api/v1/build/" + slug + "/?format=jsonp";
-		$.ajax({
-			url: url,
-			dataType: 'jsonp',
-			success: function(data){
-				if (data.objects.length > 0){
-					ele.html('<a href="http://' + slug + '.rtfd.org">' + settings.yes + '</a>');
-				} else {
-					ele.text(settings.no);
-				}
-			},
-			error: function(){
-				ele.text(settings.no);
-			}
-		});
-	});
+    return this.each(function(){
+        var ele = $(this);
+        var slug = ele.attr('rel');
+        var url = "http://readthedocs.org/api/v1/build/" + slug + "/?format=jsonp";
+        $.ajax({
+            url: url,
+            dataType: 'jsonp',
+            success: function(data){
+                if (data.objects.length > 0){
+                    ele.html('<a href="http://' + slug + '.rtfd.org">' + settings.yes + '</a>');
+                } else {
+                    ele.text(settings.no);
+                }
+            },
+            error: function(){
+                ele.text(settings.no);
+            }
+        });
+    });
 }
