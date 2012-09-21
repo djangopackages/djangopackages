367008a85a31035a40069695acd6bfee5a972e46
Daniel Greenfeld <pydanny@gmail.com>
2012-09-21 14:41:10 -0700

    Working on fixing the error #174

--- settings/heroku.py
+++ settings/heroku.py
@@ -91,7 +91,7 @@ LOGGING = {
         'django': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
-            'propagate': False,
+            'propagate': True,
         },
     }
 }
