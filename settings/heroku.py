6ad4455feaa439539eb65a023658a93cf69c0512
Daniel Greenfeld <pydanny@gmail.com>
2012-12-10 14:48:01 -0800

    Bump to django 1.4.3

--- settings/heroku.py
+++ settings/heroku.py
@@ -78,17 +78,28 @@ RESTRICT_PACKAGE_EDITORS = False
 RESTRICT_GRID_EDITORS = False
 
 ########### Errors
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
     'loggers': {
-        'django': {
+        'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
