00a460344d17a50b49ce9d1e7965424c19a029ba
Daniel Greenfeld <pydanny@gmail.com>
2013-07-25 18:26:26 +0200

    Add template caching

--- settings/heroku.py
+++ settings/heroku.py
@@ -161,3 +161,14 @@ SESSION_COOKIE_HTTPONLY = True
 SECURE_SSL_REDIRECT = True
 
 ########## end django-secure
+
+
+########## templates
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
+
+########## end templates
