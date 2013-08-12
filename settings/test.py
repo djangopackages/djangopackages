ec42270161dcbbeddeacb8add51b479adfc6a841
Daniel Greenfeld <pydanny@gmail.com>
2013-08-12 23:35:56 +0200

    Test cleanup

--- settings/test.py
+++ settings/test.py
@@ -21,6 +21,7 @@ COVERAGE_MODULE_EXCLUDES = [
     'migrations', 'fixtures', 'big_email_send$',
     'load_dev_data$', 'fix_grid_element$',
     'package_updater$', 'searchv2_build$', 'debug_toolbar',
+    'pypi_updater', 'repo_updater'
 ]
 COVERAGE_MODULE_EXCLUDES += PREREQ_APPS + ["djkombu", ]
 COVERAGE_REPORT_HTML_OUTPUT_DIR = "coverage"
