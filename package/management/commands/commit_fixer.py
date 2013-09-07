5b8f957259ad080d46a212dade7f50c2bddf1f1d
Daniel Greenfeld <pydanny@gmail.com>
2013-09-07 13:32:49 +0200

    Commit fixer for correcting missing histories thanks to @galuszkak

--- package/management/commands/commit_fixer.py
+++ package/management/commands/commit_fixer.py
@@ -37,36 +37,39 @@ class Command(BaseCommand):
                 continue
 
             self.stdout.write('Getting old GitHub commits for "%s"\n' % package)
-            data = [1, ]
-            page = 0
-            last_sha = ""
-            while len(data):
-                commit = None
-                page += 1
-                url = 'https://api.github.com/repos/{}/{}/commits?per_page=100'.format(
-                        username, repo_name
-                    )
-                if last_sha:
-                    url += "&last_sha={}".format(last_sha)
-                self.stdout.write(url + "\n")
-                r = requests.get(
-                    url=url,
+            next_url = 'https://api.github.com/repos/{}/{}/commits'.format(
+                username, repo_name
+            )
+            while next_url:
+                self.stdout.write(next_url + "\n")
+                response = requests.get(url=next_url,
                     auth=(settings.GITHUB_USERNAME, settings.GITHUB_PASSWORD)
                 )
-                if r.status_code == 200:
-                    print "COMPARE", data == r.json()
-                    data = r.json()
-                    for commit in [x['commit'] for x in data]:
-                        commit, created = Commit.objects.get_or_create(
-                            package=package,
-                            commit_date=commit['committer']['date']
-                        )
-                        last_sha = data[:-1][0]['sha']
-                    if commit:
-                        print commit.commit_date
+                if response.status_code == 200:
+                    if 'next' in response.links:
+                            next_url = response.links['next']['url']
+                    else:
+                        next_url = ''
+
+                    commit = None
+                    for commit in [x['commit'] for x in response.json()]:
+                        try:
+                            commit, created = Commit.objects.get_or_create(
+                                package=package,
+                                commit_date=commit['committer']['date']
+                            )
+                        except Commit.MultipleObjectsReturned:
+                            pass
+
+                    if commit is not None:
+                        new_commit = Commit.objects.get(pk=commit.pk)
+                        if new_commit.commit_date < timezone.now() - timezone.timedelta(days=365):
+                            self.stdout.write("Last commit recorded at {}\n".format(commit.commit_date))
+                            break
+                else:
+                    self.stdout.write("Status code is {}".format(response.status_code))
+                    break
 
                 package.last_fetched = timezone.now()
-                package.save()
-                if page > 8:
-                    break
-            self.stdout.write('Successfully fixed commits on "%s"' % package)
+            package.save()
+            self.stdout.write('Successfully fixed commits on "%s"' % package.slug)
