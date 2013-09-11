601cdfe9ca25a25acca2baced7f05951485642b7
Daniel Greenfeld <pydanny@gmail.com>
2013-09-11 07:40:46 +0200

    Search refactoring pass. Touches on #231

--- static/angular/controllers.js
+++ static/angular/controllers.js
@@ -2,12 +2,14 @@ function SearchCtrl($scope, $http) {
   $scope.model = {};
 
   $scope.init_search = function(q, url, max_weight){
+    // search page initialization
     $scope.model.q = q;
     $scope.max_weight = max_weight;
     $scope.search($scope.model, url);
   };
 
   $scope.search = function(model, url){
+    // core search page search function
     $http.get(url + '?q='+model.q).success(function(data) {
       $scope.items = data.results;
     });
