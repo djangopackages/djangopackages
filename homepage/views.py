5f92c3e80a36edf21ee8ce9af1c218d98551a833
Daniel Greenfeld <pydanny@gmail.com>
2013-11-10 18:09:25 -0800

    Add in the missing query for the home page

--- homepage/views.py
+++ homepage/views.py
@@ -90,7 +90,8 @@ def homepage(request, template_name="homepage.html"):
             "blogpost_body": blogpost_body,
             "categories": categories,
             "package_count": package_count,
-            "py3_compat": Package.objects.filter(version__supports_python3=True).select_related().distinct().count()
+            "py3_compat": Package.objects.filter(version__supports_python3=True).select_related().distinct().count(),
+            "latest_python3": Version.objects.filter(supports_python3=True).select_related("package").distinct().order_by("-created")
         }
     )
 
