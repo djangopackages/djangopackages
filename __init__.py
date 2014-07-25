3c603b177713e8266eb4881d5d325c148d3fb6c1
Daniel Greenfeld <pydanny@users.noreply.github.com>
2014-07-25 08:22:13 -0700

    Update to be about Django, not 2010-era Pinax.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
 __about__ = """
-This project comes with the bare minimum set of applications and templates
-to get you started. It includes no extra tabs, only the profile and notices
-tabs are included by default. From here you can add any extra functionality
-and applications that you would like.
+Django Packages is a directory of reusable apps, sites, tools, and more for your Django projects.
 """
