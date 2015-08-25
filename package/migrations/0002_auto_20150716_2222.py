c7bfd3012d5e86da6df9c90ef52444f349f4bce3
Daniel Roy Greenfeld <danny@eventbrite.com>
2015-08-24 21:00:11 -0700

    Added missing migrations

---
+++ package/migrations/0002_auto_20150716_2222.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('package', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='package',
+            name='repo_watchers',
+            field=models.IntegerField(default=0, verbose_name='Stars'),
+        ),
+    ]
