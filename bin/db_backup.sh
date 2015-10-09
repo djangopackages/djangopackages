f474336851539bc4ce0ad9f52b5754b806b14b46
Daniel Roy Greenfeld <pydanny@gmail.com>
2015-10-08 20:36:25 -0700

    Moar dcker

--- bin/db_backup.sh
+++ bin/db_backup.sh
@@ -2,7 +2,11 @@
 # create a db backups to backups/
 
 DOCKER_CONFIG=docker-compose-db.yml
-
 BACKUP_FILE=django$1_$(date +'%Y_%m_%dT%H_%M_%S').bak
-docker-compose -f $DOCKER_CONFIG run --rm psql pg_dump -Fc -h db -U $POSTGRES_USER -d django -f /backups/$BACKUP_FILE
+DATABASE_URL=postgres://postgresuser:mysecretpass@postgres:5432/postgresuser
+echo DATABASE_URL
+echo docker-compose -e DATABASE_URL=$DATABASE_URL run --rm psql pg_dump -Fc -h db -U postgresuser -d $DATABASE_URL -f /backups/$BACKUP_FILE
+
+docker-compose -e DATABASE_URL=$DATABASE_URL run --rm psql pg_dump -Fc -h db -U postgresuser -d $DATABASE_URL -f /backups/$BACKUP_FILE
+# docker-compose run -e DATABASE_URL=$DATABASE_URL --rm psql pg_dump -Fc -h db -U postgresuser -d $DATABASE_URL -f /backups/$BACKUP_FILE
 echo "backup $BACKUP_FILE created"
