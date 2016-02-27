72859d8f7af2bb12bb7ab10502c3f3c40f123032
Jannis Gebauer <ja.geb@me.com>
2016-02-27 12:58:38 +0100

    fixes incorrect REDIS_URL

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 # Since docker-compose relies heavily on environment variables itself for configuration, we'd have to define multiple
 # environment variables just to support cookiecutter out of the box. That makes no sense, so this little entrypoint
 # does all this for us.
-export DJANGO_CACHE_URL=redis://redis:6379/0
+export REDIS_URL=redis://redis:6379/0
 export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
 exec "$@"
--- settings/docker.py
+++ settings/docker.py
@@ -14,7 +14,7 @@ from .base import *
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "{0}/{1}".format(env.cache_url('REDIS_URL', default="redis://127.0.0.1:6379"), 0),
+        "LOCATION": env.str('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "IGNORE_EXCEPTIONS": True,  # mimics memcache behavior.
