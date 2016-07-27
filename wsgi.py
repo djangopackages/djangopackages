d9596ba3e11d889e73eb0f0eb89511f15f41e3c9
Daniel Roy Greenfeld <pydanny@gmail.com>
2016-07-26 20:16:38 -0700

    Update the pages to reflect the new direction

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Open Comparison project.
+WSGI config for Django Packages project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
