5e65dd881b8827ad40aa6489f60a45973fc4c4a1
Jannis Gebauer <ja.geb@me.com>
2017-04-11 16:23:56 +0200

    remove django-coverage, go with coverage only

--- settings/test.py
+++ settings/test.py
@@ -14,13 +14,4 @@ SERVE_MEDIA = DEBUG
 
 
 ########## TEST
-TEST_RUNNER = 'testrunner.OurCoverageRunner'
-
-COVERAGE_MODULE_EXCLUDES = PREREQ_APPS + [
-    'tests$', 'settings$', 'urls$', 'locale$',
-    'migrations', 'fixtures', 'big_email_send$',
-    'load_dev_data$', 'fix_grid_element$',
-    'package_updater$', 'searchv2_build$', 'debug_toolbar',
-    'pypi_updater', 'repo_updater',
-]
-COVERAGE_REPORT_HTML_OUTPUT_DIR = "coverage"
+TEST_RUNNER = 'testrunner.OurTestRunner'
--- testrunner.py
+++ testrunner.py
@@ -2,13 +2,8 @@
 
 from django.conf import settings
 from django.test.runner import DiscoverRunner
-from django_coverage.coverage_runner import CoverageRunner
 
 
 class OurTestRunner(DiscoverRunner):
     def build_suite(self, test_labels, *args, **kwargs):
         return super(OurTestRunner, self).build_suite(test_labels or settings.PROJECT_APPS, *args, **kwargs)
-
-
-class OurCoverageRunner(OurTestRunner, CoverageRunner):
-    pass
