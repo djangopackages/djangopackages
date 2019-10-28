4efbb38a90b0c4cac2c37c46355ef5fa3309280b
rexhepberlajolli <rexhepberlajolli@gmail.com>
2019-10-28 09:01:13 +0100

    Add debug toolbar middleware while debug is True

--- settings/base.py
+++ settings/base.py
@@ -268,7 +268,7 @@ WSGI_APPLICATION = 'wsgi.application'
 
 if DEBUG:
 
-    #MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+    MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
     INSTALLED_APPS += ('debug_toolbar',)
 
     INTERNAL_IPS = ('127.0.0.1',)
