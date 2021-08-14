3fde12afce84d2e12b725bf04d028bf1b983f2ec
Jeff Triplett <jefftriplett@users.noreply.github.com>
2021-08-14 13:16:38 -0500

    :pencil: Apply suggestions from code review

--- package/utils.py
+++ package/utils.py
@@ -1,4 +1,4 @@
-from trove_classifiers import deprecated_classifiers
+from trove_classifiers import classifiers
 from distutils.version import LooseVersion as versioner
 
 from requests.compat import quote
@@ -67,7 +67,7 @@ def normalize_license(license):
     """
     if license is None:
         return "UNKNOWN"
-    if license.strip() in deprecated_classifiers:
+    if license.strip() in classifiers:
         return license.strip()
     if len(license.strip()) > 20:
         return "Custom"
