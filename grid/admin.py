39e844925ba83a85c176226067203d12f776301e
GabbyPrecious <gabbyprecious2000@gmail.com>
2021-08-14 22:19:51 +0100

    admin changes

--- grid/admin.py
+++ grid/admin.py
@@ -12,11 +12,25 @@ class GridAdmin(VersionAdmin):
     list_display_links = ('title',)
     list_display = ('title', 'header',)
     list_editable = ('header',)
+    raw_id_fields = ["packages"]
+    search_fields = ("title", "slug", "packages",)
     inlines = [
         GridPackageInline,
     ]
 
-admin.site.register(Element, VersionAdmin)
-admin.site.register(Feature, VersionAdmin)
+class ElementAdmin(VersionAdmin):
+    raw_id_fields = ["grid_package", "feature"]
+    search_fields = ("grid_package", "feature",)
+
+class FeatureAdmin(VersionAdmin):
+    raw_id_fields = ["grid"]
+    search_fields = ("grid", "title",)
+
+class GridPackageAdmin(VersionAdmin):
+    raw_id_fields = ["grid"]
+    search_fields = ("grid", "package",)
+
+admin.site.register(Element, ElementAdmin)
+admin.site.register(Feature, FeatureAdmin)
 admin.site.register(Grid, GridAdmin)
-admin.site.register(GridPackage, VersionAdmin)
+admin.site.register(GridPackage, GridPackageAdmin)
