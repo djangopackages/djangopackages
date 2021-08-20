9320db03ed24b8d797dadf2180acb69ad79fdd7e
Jeff Triplett <jeff.triplett@gmail.com>
2021-08-20 11:16:09 -0500

    :tractor: Admin refactor to add raw_id_fields and other options
    Fixes #663

--- package/admin.py
+++ package/admin.py
@@ -6,18 +6,28 @@ from package.models import Category, Package, PackageExample, Commit, Version
 
 class PackageExampleInline(admin.TabularInline):
     model = PackageExample
+    raw_id_fields = ["created_by"]
 
 
+@admin.register(Commit)
+class CommitAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "commit_date"]
+    list_filter = ("package",)
+    raw_id_fields = ["package"]
+
+
+@admin.register(Package)
 class PackageAdmin(VersionAdmin):
-
     save_on_top = True
-    search_fields = ("title",)
-    list_filter = ("category",)
-    list_display = ("title", "created", )
+    search_fields = ["title"]
+    list_filter = ["category"]
+    list_display = ["title", "category", "created"]
     date_hierarchy = "created"
+    raw_id_fields = ["usage"]
     inlines = [
         PackageExampleInline,
     ]
+    readonly_fields = ["created_by", "last_modified_by"]
     fieldsets = (
         (None, {
             "fields": ("title", "slug", "category", "pypi_url", "repo_url", "usage", "created_by", "last_modified_by",)
@@ -29,22 +39,20 @@ class PackageAdmin(VersionAdmin):
     )
 
 
-class CommitAdmin(admin.ModelAdmin):
-    list_filter = ("package",)
-
-
-class VersionLocalAdmin(admin.ModelAdmin):
-    search_fields = ("package__title",)
-
-
+@admin.register(PackageExample)
 class PackageExampleAdmin(admin.ModelAdmin):
+    list_display = ["title",]
+    raw_id_fields = ["package"]
+    readonly_fields = ["created_by"]
+    search_fields = ["title"]
 
-    list_display = ("title", )
-    search_fields = ("title",)
+
+@admin.register(Version)
+class VersionLocalAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "license", "hidden", "supports_python3", "created"]
+    list_filter = ["hidden", "supports_python3", "created", "development_status"]
+    ordering = ["-created"]
+    search_fields = ["package__title"]
 
 
 admin.site.register(Category, VersionAdmin)
-admin.site.register(Package, PackageAdmin)
-admin.site.register(Commit, CommitAdmin)
-admin.site.register(Version, VersionLocalAdmin)
-admin.site.register(PackageExample, PackageExampleAdmin)
