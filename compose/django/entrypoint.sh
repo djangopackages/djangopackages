3fa9477ab2f2c92c48925d3092302998eca23e0c
Jeff Triplett <jeff.triplett@gmail.com>
2021-08-22 01:31:32 -0500

    Revert ":gear: Settings update to make pointing to a new DB easier"
    This reverts commit 05f2cd4ac43f34b39719fd0f96847bf26f4b5bda.

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -8,7 +8,7 @@ python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="$POSTGRES_HOST", port="$POSTGRES_PORT", dbname="$POSTGRES_DB")
+    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
@@ -23,5 +23,5 @@ done
 >&2 echo "Postgres is up - continuing..."
 
 export REDIS_URL=redis://redis:6379/0
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
 exec $cmd
