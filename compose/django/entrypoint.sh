2a563de960b79ef9ad232d27301780942e10e900
Jeff Triplett <jeff.triplett@gmail.com>
2021-09-22 10:11:18 -0500

    :gear: Fixing dbname to reference POSTGRES_DB instead of *_USER

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -8,7 +8,7 @@ python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+    conn = psycopg2.connect(dbname="$POSTGRES_DB", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
@@ -23,5 +23,5 @@ done
 >&2 echo "Postgres is up - continuing..."
 
 export REDIS_URL=redis://redis:6379/0
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_DB
 exec $cmd
