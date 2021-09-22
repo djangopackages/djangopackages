7d33a613dfc4df9ce48cded8a1d4d046dd6a1352
Jeff Triplett <jeff.triplett@gmail.com>
2021-09-22 14:36:36 -0500

    :gear: Setups up db connection info

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -8,7 +8,7 @@ python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_DB", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+    conn = psycopg2.connect(dbname="$POSTGRES_DB", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="$POSTGRES_HOST", port="$POSTGRES_PORT")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
@@ -23,5 +23,5 @@ done
 >&2 echo "Postgres is up - continuing..."
 
 export REDIS_URL=redis://redis:6379/0
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_DB
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB
 exec $cmd
