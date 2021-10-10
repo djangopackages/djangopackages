ae9721d9dede76c6411b338c25fb9e994f5f1a15
Jeff Triplett <jeff.triplett@gmail.com>
2021-10-10 16:45:30 -0500

    :gear: Drops sentry traces_sample_rate because it's set too high

--- settings/docker.py
+++ settings/docker.py
@@ -59,7 +59,7 @@ if SENTRY_DSN:
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.2,
 
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
