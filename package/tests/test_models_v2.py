fbd545112347674afb0b297f6f08a943865203d6
Jeff Triplett <jeff.triplett@gmail.com>
2022-02-19 22:02:58 -0600

    :green_heart: Fills in some test info

--- package/tests/test_models_v2.py
+++ package/tests/test_models_v2.py
@@ -1,17 +1,23 @@
 def test_category(category):
-    assert True
+    assert str(category) == f"{category.title}"
 
 
 def test_commit(commit):
-    assert True
+    assert str(commit) == f"Commit for '{commit.package.title}' on {commit.commit_date}"
 
 
 def test_package(package):
-    assert True
+    assert str(package) == f"{package.title}"
+    assert package.is_deprecated is False
+    assert package.get_pypi_uri()
+    assert package.get_pypi_json_uri()
+    assert package.pypi_name()
+    assert package.repo
+    assert package.last_updated
 
 
 def test_package_example(package_example):
-    assert True
+    assert str(package_example) == f"{package_example.title}"
 
 
 # def test_version(version):
