ec742990570329d0bdd84af286d8f7b824ba09ce
Jeff Triplett <jeff.triplett@gmail.com>
2022-03-03 21:07:14 -0600

    :bug: Make pypi-updater more verbose and exclude last 24 hours by default

--- package/management/commands/pypi_updater.py
+++ package/management/commands/pypi_updater.py
@@ -2,6 +2,7 @@ import djclick as click
 import logging
 
 from django.conf import settings
+from django.db.models import Q
 from django.utils import timezone
 from rich import print
 
@@ -12,13 +13,24 @@ logger = logging.getLogger(__name__)
 
 
 @click.command()
-def command():
+@click.option("--all", default=False)
+def command(all):
     """Updates all the packages in the system by checking against their PyPI data."""
     count = 0
     count_updated = 0
-    packages = Package.objects.all().order_by("last_fetched")
+
+    now = timezone.now()
+    if not all:
+        now = now - timezone.timedelta(hours=24)
+
+    packages = (
+        Package.objects.exclude(Q(pypi_url="") | Q(pypi_url__isnull=True))
+        .filter(Q(last_fetched__lt=now) | Q(last_fetched__isnull=True))
+        .order_by("last_fetched")
+    )
     print(f"{packages.count()} to update")
     for package in packages.iterator():
+        print(f"{package} | {package.last_fetched} | {package.pypi_url}")
         try:
             if package.fetch_pypi_data():
                 count_updated += 1
