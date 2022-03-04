32a2d8c362d5ca15b808a3d66ce5b22194af68d1
Jeff Triplett <jeff.triplett@gmail.com>
2022-03-03 20:42:37 -0600

    :bug: Update pypi_updater to be more verbose

--- package/management/commands/pypi_updater.py
+++ package/management/commands/pypi_updater.py
@@ -3,6 +3,7 @@ import logging
 
 from django.conf import settings
 from django.utils import timezone
+from rich import print
 
 from core.utils import healthcheck
 from package.models import Package
@@ -15,11 +16,16 @@ def command():
     """Updates all the packages in the system by checking against their PyPI data."""
     count = 0
     count_updated = 0
-    for package in Package.objects.all().order_by("last_fetched").iterator():
-        if package.fetch_pypi_data():
-            count_updated += 1
-            package.last_fetched = timezone.now()
-            package.save()
+    packages = Package.objects.all().order_by("last_fetched")
+    print(f"{packages.count()} to update")
+    for package in packages.iterator():
+        try:
+            if package.fetch_pypi_data():
+                count_updated += 1
+                package.last_fetched = timezone.now()
+                package.save()
+        except Exception as e:
+            print(f"[red]{package} :: {e}[/red]")
         count += 1
         msg = f"{count}. {count_updated}. {package}"
         logger.info(msg)
