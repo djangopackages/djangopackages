47e40aee1ff65483f526e6b5b90ce86da2201ff0
Jeff Triplett <jeff.triplett@gmail.com>
2022-03-16 20:43:13 -0500

    :green_hearts: Switch to VCR support

--- package/tests/test_repos_github.py
+++ package/tests/test_repos_github.py
@@ -1,5 +1,7 @@
 import pytest
 
+from github3.exceptions import NotFoundError
+
 from package.repos.github import GitHubHandler
 from package.models import Commit
 
@@ -9,20 +11,23 @@ def github_handler():
     return GitHubHandler()
 
 
-def test_fetch_commits(github_handler, package):
+@pytest.mark.vcr()
+def test_github_fetch_commits(github_handler, package):
     assert Commit.objects.count() == 0
     github_handler.fetch_commits(package)
     assert Commit.objects.count() > 0
 
 
-def test_fetch_metadata(github_handler, package):
+@pytest.mark.vcr()
+def test_github_fetch_metadata(github_handler, package):
     # Currently a live tests that access github
     package = github_handler.fetch_metadata(package)
     assert package.repo_description == "Django Enhancement Proposals"
     assert package.repo_watchers > 100
 
 
-def test_fetch_metadata_archived_repo(github_handler, package_archived):
+@pytest.mark.vcr()
+def test_github_fetch_metadata_archived_repo(github_handler, package_archived):
     # test what happens when we reference an archived repository
     assert package_archived.date_repo_archived is None
     package = github_handler.fetch_metadata(package_archived)
@@ -32,12 +37,17 @@ def test_fetch_metadata_archived_repo(github_handler, package_archived):
     assert package.date_repo_archived is not None
 
 
-def test_fetch_metadata_unsupported_repo(github_handler, package_invalid):
+@pytest.mark.vcr()
+def test_github_fetch_metadata_unsupported_repo(github_handler, package_invalid):
     # test what happens when setting up an unsupported repository
-    assert package_invalid.repo_url == "https://example.com"
-    package = github_handler.fetch_metadata(package_invalid)
+    assert (
+        package_invalid.repo_url == "https://github.com/djangopackages/does-not-exist"
+    )
 
-    assert package.repo_description == ""
-    assert package.repo_watchers == 0
-    package_invalid.fetch_commits()
-    assert package.commit_set.count() == 0
+    with pytest.raises(NotFoundError):
+        package = github_handler.fetch_metadata(package_invalid)
+
+    # assert package.repo_description == ""
+    # assert package.repo_watchers == 0
+    # package_invalid.fetch_commits()
+    # assert package.commit_set.count() == 0
