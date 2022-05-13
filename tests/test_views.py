1a0774d38591a1aa90785ad3ccab526bde1c4753
Jeff Triplett <jeff.triplett@gmail.com>
2022-05-13 09:24:38 -0500

    :green_heart: Adds tests for static pages

---
+++ tests/test_views.py
@@ -0,0 +1,46 @@
+def test_about(db, tp):
+    url = tp.reverse("about")
+    response = tp.client.get(url)
+    assert response.status_code == 200
+
+
+def test_faq(db, tp):
+    url = tp.reverse("faq")
+    response = tp.client.get(url)
+    assert response.status_code == 200
+
+
+def test_funding(db, tp):
+    url = tp.reverse("funding")
+    response = tp.client.get(url)
+    assert response.status_code == 200
+
+
+def test_help(db, tp):
+    url = tp.reverse("help")
+    response = tp.client.get(url)
+    assert response.status_code == 200
+
+
+def test_open(db, tp):
+    url = tp.reverse("open")
+    response = tp.client.get(url)
+    assert response.status_code == 200
+
+
+def test_sitemap(db, tp):
+    url = tp.reverse("sitemap")
+    response = tp.client.get(url)
+    assert response.status_code == 200
+
+
+def test_syndication(db, tp):
+    url = tp.reverse("syndication")
+    response = tp.client.get(url)
+    assert response.status_code == 200
+
+
+def test_terms(db, tp):
+    url = tp.reverse("terms")
+    response = tp.client.get(url)
+    assert response.status_code == 200
