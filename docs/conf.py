2b583dfff1a9a1ba6a3c697bd002f9944a94b2cd
Jeff Triplett <jefftriplett@users.noreply.github.com>
2022-10-27 19:53:56 -0700

    :gear: Setups docs for an updated theme (#860)

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ import time
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    "sphinx_material",
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
     "sphinx.ext.coverage",
@@ -109,7 +110,8 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+# html_theme = "default"
+html_theme = "sphinx_material"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
