7ff233905eeb051a4d0e4edaccb35170cb484fd5
Jeff Triplett <jeff.triplett@gmail.com>
2022-11-26 23:25:00 -0600

    :sparkles: Adds check_package_examples

---
+++ package/management/commands/check_package_examples.py
@@ -0,0 +1,43 @@
+import djclick as click
+import requests
+
+from package.models import PackageExample
+from requests.exceptions import ConnectionError
+from requests.exceptions import HTTPError
+from requests.exceptions import ReadTimeout
+from requests.exceptions import SSLError
+from rich import print
+
+
+@click.command()
+@click.option("--limit", default=None, type=int)
+def command(limit):
+    print("[bold]raw stats[/bold]")
+    print(f"{PackageExample.objects.filter(active=None).count()=}")
+    print(f"{PackageExample.objects.filter(active=True).count()=}")
+    print(f"{PackageExample.objects.filter(active=False).count()=}")
+    print()
+
+    package_examples = (
+        PackageExample.objects.exclude(active=False).order_by("url").distinct("url")
+    )
+
+    if limit:
+        package_examples = package_examples[:limit]
+
+    print(f"{package_examples.count()=}")
+
+    for example in package_examples:
+        url = f"{example.url}"
+        if not url.startswith("http"):
+            # we are going to assume "https" here for security reasons
+            url = f"https://{url}"
+
+        try:
+            response = requests.head(url, timeout=2, verify=False)
+            response.raise_for_status()
+        except (ConnectionError, HTTPError, ReadTimeout, SSLError):
+            print(f"[red]marking {example.url} as inactive[/red]")
+            PackageExample.objects.filter(url=example.url).update(active=False)
+
+        # TODO: Possibly do a SPAM check on the domain...
