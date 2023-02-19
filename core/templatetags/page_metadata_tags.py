9e5138b390e942940b44b892c821ae55a1c71007
Jeff Triplett <jeff.triplett@gmail.com>
2023-02-18 19:35:27 -0600

    :tractor: Updates page_metadata template tag

--- core/templatetags/page_metadata_tags.py
+++ core/templatetags/page_metadata_tags.py
@@ -1,12 +1,19 @@
 from django import template
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
+from urllib.parse import quote_plus
 
 register = template.Library()
 
 
 @register.inclusion_tag("includes/page_metadata.html", takes_context=True)
-def page_metadata(context, page_title=None, page_description=None, page_keywords=None):
+def page_metadata(
+    context,
+    page_title: str = None,
+    page_description: str = None,
+    page_keywords: str = None,
+    og_image_url: str = None,
+):
     request = context["request"]
     site_title = getattr(settings, "SITE_TITLE", "Django Packages")
     base_url = f"{request.scheme}://{request.get_host()}"
@@ -17,14 +24,24 @@ def page_metadata(context, page_title=None, page_description=None, page_keywords
     page_description = page_description or _(
         "Django Packages stores information on fetched packages "
         "and provides easy comparison tools for them. "
-        "Public APIs include PyPI, Github, and BitBucket."
+        "Public APIs include PyPI, GitHub, and BitBucket."
     )
+
+    if og_image_url is None:
+        og_image_url = f"{base_url}{settings.STATIC_URL}img/open_graph.png"
+    else:
+        url = f"{quote_plus(base_url + og_image_url)}"
+        og_image_url = f"https://v1.screenshot.11ty.dev/{url}/opengraph/"
+
+    print(f"{og_image_url=}")
+
     return {
-        "site_title": site_title,
         "base_url": base_url,
-        "static_url": settings.STATIC_URL,
-        "page_url": f"{base_url}{request.path}",
-        "page_title": page_title,
+        "og_image_url": og_image_url,
         "page_description": page_description,
         "page_keywords": page_keywords,
+        "page_title": page_title,
+        "page_url": f"{base_url}{request.path}",
+        "site_title": site_title,
+        "static_url": settings.STATIC_URL,
     }
