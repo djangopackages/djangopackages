44b8d80370f6f6f7cf60b2dcbe3565706fb13fdb
Jeff Triplett <jeff.triplett@gmail.com>
2023-04-06 22:24:03 -0500

    :bug: Fixes template_tag rendering on a 500 error

--- core/templatetags/page_metadata_tags.py
+++ core/templatetags/page_metadata_tags.py
@@ -14,32 +14,38 @@ def page_metadata(
     page_keywords: str = None,
     og_image_url: str = None,
 ):
-    request = context["request"]
-    site_title = getattr(settings, "SITE_TITLE", "Django Packages")
-    base_url = f"{request.scheme}://{request.get_host()}"
-    page_title = page_title or _("Reusable apps, sites and tools directory for Django")
-    page_keywords = page_keywords or _(
-        "Django, Django Apps, Packages, Tools, Django Sites, Django Resources"
-    )
-    page_description = page_description or _(
-        "Django Packages stores information on fetched packages "
-        "and provides easy comparison tools for them. "
-        "Public APIs include PyPI, GitHub, and BitBucket."
-    )
+    try:
+        request = context["request"]
+        site_title = getattr(settings, "SITE_TITLE", "Django Packages")
+        base_url = f"{request.scheme}://{request.get_host()}"
+        page_title = page_title or _("Reusable apps, sites and tools directory for Django")
+        page_keywords = page_keywords or _(
+            "Django, Django Apps, Packages, Tools, Django Sites, Django Resources"
+        )
+        page_description = page_description or _(
+            "Django Packages stores information on fetched packages "
+            "and provides easy comparison tools for them. "
+            "Public APIs include PyPI, GitHub, and BitBucket."
+        )
 
-    if og_image_url is None:
-        og_image_url = f"{base_url}{settings.STATIC_URL}img/open_graph.png"
-    else:
-        url = f"{quote_plus(base_url + og_image_url)}"
-        og_image_url = f"https://v1.screenshot.11ty.dev/{url}/opengraph/_20230219/"
+        if og_image_url is None:
+            og_image_url = f"{base_url}{settings.STATIC_URL}img/open_graph.png"
+        else:
+            url = f"{quote_plus(base_url + og_image_url)}"
+            og_image_url = f"https://v1.screenshot.11ty.dev/{url}/opengraph/_20230219/"
 
-    return {
-        "base_url": base_url,
-        "og_image_url": og_image_url,
-        "page_description": page_description,
-        "page_keywords": page_keywords,
-        "page_title": page_title,
-        "page_url": f"{base_url}{request.path}",
-        "site_title": site_title,
-        "static_url": settings.STATIC_URL,
-    }
+        return {
+            "base_url": base_url,
+            "og_image_url": og_image_url,
+            "page_description": page_description,
+            "page_keywords": page_keywords,
+            "page_title": page_title,
+            "page_url": f"{base_url}{request.path}",
+            "site_title": site_title,
+            "static_url": settings.STATIC_URL,
+        }
+    except KeyError:
+        return {
+            "site_title": getattr(settings, "SITE_TITLE", "Django Packages"),
+            "static_url": settings.STATIC_URL,
+        }
