e802e0929107dde9dc86470abebc767d4ad70f57
Ryan Cheley <rcheley@gmail.com>
2023-12-03 09:30:47 -0800

    🔥 removed the custom script to generate docs

--- scripts/generate_rules.sh
+++
@@ -1,8 +0,0 @@
-TEMPLATE_FILE="docs-template.md"
-
-docker-compose run django cog -D class_name=DeprecatedRule -o docs/docs/rules/deprecated.md -d $TEMPLATE_FILE
-docker-compose run django cog -D class_name=DescriptionRule -o docs/docs/rules/description.md -d $TEMPLATE_FILE
-docker-compose run django cog -D class_name=DownloadsRule -o docs/docs/rules/downloads.md -d $TEMPLATE_FILE
-docker-compose run django cog -D class_name=ForkRule -o docs/docs/rules/fork.md -d $TEMPLATE_FILE
-docker-compose run django cog -D class_name=UsageCountRule -o docs/docs/rules/usage_count.md -d $TEMPLATE_FILE
-docker-compose run django cog -D class_name=WatchersRule -o docs/docs/rules/watchers.md -d $TEMPLATE_FILE
