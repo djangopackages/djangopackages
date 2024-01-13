e0a9c4414f99746a37dced110661682bac7fd056
Jeff Triplett <jeff.triplett@gmail.com>
2024-01-12 19:42:16 -0600

    :green_heart: Updates tests

--- package/tests/test_views.py
+++ package/tests/test_views.py
@@ -15,7 +15,7 @@ def test_python3_list(db, django_assert_num_queries, tp):
     assert Package.objects.count() == 4
 
     url = tp.reverse("py3_compat")
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(10):
         response = tp.client.get(url)
 
     assert response.status_code == 200
@@ -28,7 +28,7 @@ def test_python3_list_blank_sort_empty(db, django_assert_num_queries, tp):
     assert Package.objects.count() == 4
 
     url = tp.reverse("py3_compat")
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(10):
         response = tp.client.get(url, data={"dir": ""})
     assert response.status_code == 200
 
@@ -40,7 +40,7 @@ def test_python3_list_blank_sort_asc(db, django_assert_num_queries, tp):
     assert Package.objects.count() == 4
 
     url = tp.reverse("py3_compat")
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(10):
         response = tp.client.get(url, data={"dir": "asc"})
     assert response.status_code == 200
 
@@ -52,7 +52,7 @@ def test_python3_list_blank_sort_desc(db, django_assert_num_queries, tp):
     assert Package.objects.count() == 4
 
     url = tp.reverse("py3_compat")
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(10):
         response = tp.client.get(url, data={"dir": "desc"})
     assert response.status_code == 200
 
@@ -64,7 +64,7 @@ def test_python3_list_blank_sort_by_valid_field(db, django_assert_num_queries, t
     assert Package.objects.count() == 4
 
     url = tp.reverse("py3_compat")
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(10):
         response = tp.client.get(url, data={"dir": "desc", "sort": "repo_watchers"})
     assert response.status_code == 200
 
@@ -76,7 +76,7 @@ def test_python3_list_blank_sort_by_bad_field(db, django_assert_num_queries, tp)
     assert Package.objects.count() == 4
 
     url = tp.reverse("py3_compat")
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(10):
         response = tp.client.get(url, data={"dir": "desc", "sort": "doesnotexist"})
     assert response.status_code == 200
 
@@ -102,7 +102,7 @@ class FunctionalPackageTest(TestCase):
 
     def test_package_detail_view(self):
         url = reverse("package", kwargs={"slug": "testability"})
-        with self.assertNumQueries(11):
+        with self.assertNumQueries(16):
             response = self.client.get(url)
         self.assertTemplateUsed(response, "package/package.html")
         p = Package.objects.get(slug="testability")
@@ -240,7 +240,7 @@ class FunctionalPackageTest(TestCase):
         self.assertTemplateUsed(response, "package/edit_example.html")
         self.assertNotContains(response, "example-delete-btn")
 
-        with self.assertNumQueries(5):
+        with self.assertNumQueries(10):
             response = self.client.post(
                 url,
                 {
@@ -440,12 +440,12 @@ class PackagePermissionTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
         settings.RESTRICT_PACKAGE_EDITORS = True
-        with self.assertNumQueries(5):
+        with self.assertNumQueries(10):
             response = self.client.get(self.test_add_url)
         self.assertEqual(response.status_code, 403)
 
     def test_add_package_permission_fail(self):
-        with self.assertNumQueries(5):
+        with self.assertNumQueries(10):
             response = self.client.get(self.test_add_url)
         self.assertEqual(response.status_code, 403)
 
@@ -459,7 +459,7 @@ class PackagePermissionTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_edit_package_permission_fail(self):
-        with self.assertNumQueries(5):
+        with self.assertNumQueries(10):
             response = self.client.get(self.test_edit_url)
         self.assertEqual(response.status_code, 403)
 
@@ -476,7 +476,7 @@ class PackagePermissionTest(TestCase):
 def test_category_view(db, django_assert_num_queries, tp):
     initial_data.load()
 
-    with django_assert_num_queries(23):
+    with django_assert_num_queries(28):
         response = tp.client.get("/categories/apps/")
     assert "apps" in str(response.content)
 
@@ -484,7 +484,7 @@ def test_category_view(db, django_assert_num_queries, tp):
 def test_grid_package_list(db, django_assert_num_queries, tp):
     initial_data.load()
 
-    with django_assert_num_queries(19):
+    with django_assert_num_queries(24):
         url = tp.reverse("grid_packages", slug="testing")
         response = tp.client.get(url)
 
