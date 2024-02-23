0d715c4ceca956e4729c74bac5a6476ee59af8c2
Jeff Triplett <jeff.triplett@gmail.com>
2024-02-23 15:44:21 -0600

    :sparkles: Adds a blog app to bootstrap changelogs

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "published_date"]
+    raw_id_fields = ["author"]
