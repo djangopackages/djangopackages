15c269b88450e719d719216b9ed7d21e9ae271c5
Jeff Triplett <jeff.triplett@gmail.com>
2024-02-24 14:51:10 -0600

    :sparkles: Adds post list and detail views and templates

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+from blog.models import Post
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+
+class PostList(ListView):
+    context_object_name = "posts"
+    model = Post
+    ordering = ["-published_date"]
+    template_name = "blog/post_list.html"
