e4f508f7ef00d49b7f37313ddc1510c23833f5ae
Jeff Triplett <jeff.triplett@gmail.com>
2024-04-19 17:22:55 -0500

    :pencil: Swap out runserver with tailwind runserver

--- dockerfiles/django/start-dev.sh
+++ dockerfiles/django/start-dev.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# python -m manage tailwind --skip-checks build
+
 python -m manage migrate --noinput
-python -m manage runserver 0.0.0.0:8000
+
+python -m manage tailwind --skip-checks runserver 0.0.0.0:8000
