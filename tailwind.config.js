2806dc8f0889b5031ec21264cf3b0e39a3c8ca25
Jeff Triplett <jeff.triplett@gmail.com>
2024-04-27 13:22:48 -0500

    :fire: Disable tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,7 +43,7 @@ module.exports = {
   },
   plugins: [
     require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
+    // require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/container-queries"),
     plugin(function ({ addVariant }) {
