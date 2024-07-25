17326afc28a45ae512228c541092aae16be9a9ac
Farhan <farhanalirazaazeemi@gmail.com>
2024-07-25 22:03:52 +0500

    partials and loading favorite state

--- favorites/views.py
+++ favorites/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import View
-from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.conf import settings
 from django_htmx.http import HttpResponseClientRedirect
@@ -22,6 +21,29 @@ class FavoritePackage(View):
         _, created = Favorite.objects.get_or_create(
             package=package, favorited_by=request.user
         )
-        return HttpResponse(
-            '<span class="glyphicon glyphicon-heart "></span> Favorited'
+        return render(
+            request,
+            "package/partials/favorites.html#unfavorite_btn",
+            {"package": package},
+        )
+
+
+class UnFavoritePackage(View):
+    def post(self, request, id):
+        if not request.user.is_authenticated:
+            return HttpResponseClientRedirect(settings.LOGIN_URL)
+
+        try:
+            package = Package.objects.get(id=id)
+        except Package.DoesNotExist:
+            messages.error(request, "Package does not exist")
+            return redirect("/")
+
+        # _, created = Favorite.objects.get_or_create(
+        #     package=package, favorited_by=request.user
+        # )
+        return render(
+            request,
+            "package/partials/favorites.html#unfavorite_btn",
+            {"package": package},
         )
--- settings/base.py
+++ settings/base.py
@@ -165,6 +165,7 @@ PREREQ_APPS = [
     "social_django",
     "waffle",
     "django_q",
+    "template_partials",
 ]
 
 INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS
