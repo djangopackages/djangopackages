c328d5d67d0a240ae5b1cba100475998f6db16cb
Jeff Triplett <jeff.triplett@gmail.com>
2024-10-25 15:43:41 -0500

    :tractor: Switch out to looseversion

--- package/utils.py
+++ package/utils.py
@@ -1,9 +1,7 @@
-import packaging.version as versioner
-
 from django.db import models
+from looseversion import LooseVersion
 from requests.compat import quote
 from trove_classifiers import classifiers
-
 
 # this is gross, but requests doesn't import quote_plus into compat,
 # so we re-implement it here
@@ -43,7 +41,7 @@ def get_version(package):
 def get_pypi_version(package):
     versions = []
     for v_str in package.version_set.values_list("number", flat=True):
-        v = versioner(v_str)
+        v = LooseVersion(v_str)
         comparable = True
         for elem in v.version:
             if isinstance(elem, str):
