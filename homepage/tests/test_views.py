99c50c250c7c2a815866df6a4cda26177d6dfb22
farhan <farhanalirazaazeemi@gmail.com>
2025-01-09 16:42:45 +0500

    update homepage test

--- homepage/tests/test_views.py
+++ homepage/tests/test_views.py
@@ -43,7 +43,7 @@ from django.urls.exceptions import NoReverseMatch
 
 def test_homepage(db, tp, django_assert_num_queries):
     url = tp.reverse("home")
-    with django_assert_num_queries(15):
+    with django_assert_num_queries(16):
         response = tp.client.get(url)
     assert response.status_code == 200
 
