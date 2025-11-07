8e066ed5ac45a9ccc88f2d50abf432c50d4de9ac
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2025-11-07 16:12:13 -0600

    Bug Fixes

--- package/repos/forgejo.py
+++ package/repos/forgejo.py
@@ -187,7 +187,8 @@ class ForgejoHandler(BaseHandler):
 
         package.repo_description = repo.description
         package.repo_forks = repo.forks_count
-        package.repo_watchers = repo.stars_count
+        package.repo_watchers = repo.watchers_count
+        # package.stargazers_count = repo.stars_count
         package.save()
 
         return package
