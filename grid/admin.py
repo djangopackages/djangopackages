8969828ac1827923548ffdf268f4802a9262d1de
saadmk11 <saad.mk112@gmail.com>
2026-02-14 14:37:36 +0600

    Fix Grid App Admin Search

--- grid/admin.py
+++ grid/admin.py
@@ -25,16 +25,20 @@ class GridAdmin(VersionAdmin):
 @admin.register(Element)
 class ElementAdmin(VersionAdmin):
     raw_id_fields = ["grid_package", "feature"]
-    search_fields = ["grid_package", "feature"]
+    search_fields = [
+        "grid_package__grid__title",
+        "grid_package__package__title",
+        "feature__title",
+    ]
 
 
 @admin.register(Feature)
 class FeatureAdmin(VersionAdmin):
     raw_id_fields = ["grid"]
-    search_fields = ["grid", "title"]
+    search_fields = ["grid__title", "title"]
 
 
 @admin.register(GridPackage)
 class GridPackageAdmin(VersionAdmin):
     raw_id_fields = ["grid", "package"]
-    search_fields = ["grid", "package"]
+    search_fields = ["grid__title", "package__title"]
